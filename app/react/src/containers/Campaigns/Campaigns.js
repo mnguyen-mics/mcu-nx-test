3ee2b1bfe829f37360d8946d1fc8f4170c95691f
Emilien Sanchez <emilien.sanchez@gadz.org>
2017-04-06 12:47:55 +0200

    removing useless elements

--- app/react/src/containers/Campaigns/Campaigns.js
+++ app/react/src/containers/Campaigns/Campaigns.js
@@ -6,28 +6,6 @@ import CampaignsSidebar from './CampaignsSidebar';
 import { ScrollComponent } from '../../components/ScrollComponent';
 import { LabelListView } from '../../components/LabelListView';
 
-const items = [
-  {
-    key: 1,
-    value: 'tag 1',
-    isClosable: true,
-  },
-  {
-    key: 2,
-    value: 'tag 2',
-    isClosable: true,
-  },
-  {
-    key: 3,
-    value: 'tag 3',
-    isClosable: false,
-    icon: 'link',
-  }
-];
-
-const returnFunc = (e) => {
-  console.log(e);
-};
 
 class Campaigns extends Component {
 
