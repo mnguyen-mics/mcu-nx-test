969429347158572dc6e47693a38845b0fc961573
Loïc Girault <lgirault@mediarithmics.com>
2017-04-06 14:15:26 +0200

    Add labelled input with toolbox

---
+++ app/react/src/components/Forms/GridInput/InternalGridInput.js
@@ -0,0 +1,31 @@
+import React, { Component, PropTypes } from 'react';
+
+class InternalGridInput extends Component {
+  render() {
+
+    const {
+      input,
+      meta: { error, touched },
+      className,
+      ...other
+    } = this.props;
+
+    const classValue = (touched && error) ? 'mics-gridinput-input-error' : className;
+
+    return (
+      <div > <input {...input} className={classValue} type="text" {...other} /> </div>
+    );
+  }
+}
+
+InternalGridInput.defaultProps = {
+  input: ['form-control']
+};
+
+InternalGridInput.propTypes = {
+  input: PropTypes.object, // eslint-disable-line react/forbid-prop-types
+  meta: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+  className: PropTypes.string.isRequired
+};
+
+export default InternalGridInput;
--- app/react/src/components/Forms/index.js
+++ app/react/src/components/Forms/index.js
@@ -1,5 +1,7 @@
 import Input from './Inputs/Input';
+import GridInput from './GridInput/GridInput';
 
 export {
-  Input
+  Input,
+  GridInput
 };
