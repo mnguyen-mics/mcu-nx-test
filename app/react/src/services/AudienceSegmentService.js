c7723e4eeae44dbf2709ccb6d3ba1006b2c6ef42
Audrey Lambert <audreylambert@Audreys-MacBook-Pro.local>
2017-08-09 17:33:38 +0200

    [FIX] Changed .map to .then

--- app/react/src/services/AudienceSegmentService.js
+++ app/react/src/services/AudienceSegmentService.js
@@ -19,7 +19,7 @@ const getSegment = (segmentId, options = {}) => {
     ...options,
   };
 
-  return ApiService.getRequest(endpoint, params).map(res => res.data);
+  return ApiService.getRequest(endpoint, params).then(res => res.data);
 };
 
 const createOverlap = (datamartId, segmentId) => {
