332c310a3bec9b58502e5595897b250530a8e33e
Emilien <esanchez@mediarithmics.com>
2017-08-22 14:30:20 +0200

    add angular rerouting

--- app/angular/src/core/datamart/monitoring/ViewAllController.js
+++ app/angular/src/core/datamart/monitoring/ViewAllController.js
@@ -4,8 +4,11 @@ define(['./module'], function (module) {
 
 
   module.controller('core/datamart/monitoring/ViewAllController', [
-    '$scope', 'core/common/auth/Session',
-    function($scope, Session) {
+    '$scope', 'core/common/auth/Session', '$location',
+    function($scope, Session, $location) {
+
+      // redirect to v2
+      $location.path(Session.getV2WorkspacePrefixUrl() + '/audience/timeline');
 
       if (Session.cookies && Session.cookies.mics_vid){
         $scope.myTimelineHref = "#" + Session.getWorkspacePrefixUrl() + "/datamart/users/user_agent_id/vec:" + Session.cookies.mics_vid + "?live=true";
