3368a12f7d20be573ab6a758c74d34dc90252435
Maxime Leuger <mleuger@mediarithmics.com>
2017-09-26 15:01:23 +0200

    fix tableView bug + fix CTR percentages

--- app/react/src/containers/Campaigns/Display/Dashboard/Common/DisplayCampaignAdTable.js
+++ app/react/src/containers/Campaigns/Display/Dashboard/Common/DisplayCampaignAdTable.js
@@ -198,7 +198,7 @@ class DisplayCampaignAdTable extends Component {
         key: 'ctr',
         isVisibleByDefault: true,
         isHideable: true,
-        render: text => renderMetricData(text / 100, '0,00 %'),
+        render: text => renderMetricData(parseFloat(text) / 100, '0.000 %'),
         sorter: (a, b) => sorter(a, b, 'ctr'),
       },
       {
