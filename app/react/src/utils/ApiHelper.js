2348c7b39ccb7b95373dbed5b28f814a550f5320
zobione <esanchez@medairithmics.com>
2017-10-24 16:54:37 +0200

    fix issues

--- app/react/src/utils/ApiHelper.js
+++ app/react/src/utils/ApiHelper.js
@@ -38,19 +38,19 @@ export const takeLatest = functionReturningPromise => {
 };
 
 export const makeCancelable = (promise) => {
-  let hasCanceled_ = false; // eslint-disable-line no-underscore-dangle
+  let hasCanceled = false;
 
   const wrappedPromise = new Promise((resolve, reject) => {
     promise.then(
-      (val) => hasCanceled_ ? reject({ isCanceled: true }) : resolve(val), // eslint-disable-line no-confusing-arrow
-      error => hasCanceled_ ? reject({ isCanceled: true }) : reject(error) // eslint-disable-line no-confusing-arrow
+      val => hasCanceled ? reject({ isCanceled: true }) : resolve(val), // eslint-disable-line no-confusing-arrow
+      error => hasCanceled ? reject({ isCanceled: true }) : reject(error) // eslint-disable-line no-confusing-arrow
     );
   });
 
   return {
     promise: wrappedPromise,
     cancel() {
-      hasCanceled_ = true;
+      hasCanceled = true;
     },
   };
 };
