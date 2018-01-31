3d1975af85cab3481b67a23ff2b175cc21a32372
Nicolas Dussart <ndussart@mediarithmics.com>
2018-01-31 18:01:50 +0100

    Fix Status Filter

--- app/react/src/services/CampaignService.ts
+++ app/react/src/services/CampaignService.ts
@@ -1,12 +1,29 @@
 import ApiService, { DataListResponse } from './ApiService';
 import { CampaignResource } from '../models/campaign/CampaignResource';
+import { PaginatedApiParam } from '../utils/ApiHelper';
+import { CampaignStatus } from '../models/campaign/constants/index';
 
-function getCampaigns(organisationId: string, campaignType: 'DISPLAY' | 'EMAIL'): Promise<DataListResponse<CampaignResource>> {
+interface GetCampaignsOptions extends PaginatedApiParam {
+  administration_id?: string;
+  scope?: string;
+  keywords?: string[];
+  status?: CampaignStatus[];
+  archived?: boolean;
+  label_ids?: string[];
+  order_by?: string[];
+}
+
+function getCampaigns(
+  organisationId: string,
+  campaignType: 'DISPLAY' | 'EMAIL',
+  options: GetCampaignsOptions = {},
+): Promise<DataListResponse<CampaignResource>> {
   const endpoint = 'campaigns';
 
   const params = {
     organisation_id: organisationId,
     campaign_type: campaignType,
+    ...options,
   };
 
   return ApiService.getRequest(endpoint, params);
