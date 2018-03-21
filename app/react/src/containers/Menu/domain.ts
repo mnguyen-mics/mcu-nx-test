42b2e4c756a35b718dd66951d0186b25e13e572f
zobione <esanchez@medairithmics.com>
2018-03-21 10:37:14 +0100

    Fix @ndussart remarks

--- app/react/src/containers/Menu/domain.ts
+++ app/react/src/containers/Menu/domain.ts
@@ -1,8 +1,10 @@
+import { FormattedMessage } from "react-intl";
+
 export interface NavigatorMenuElement {
     key: string;
     iconType?: string;
     path: string;
-    translation: {id: string, defaultMessage: string};
+    translation: FormattedMessage.MessageDescriptor;
     translationId?: string;
     legacyPath?: boolean;
     subMenuItems?: NavigatorMenuElement[];
