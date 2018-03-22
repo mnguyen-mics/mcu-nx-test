84982ffcb50e9323b8e0de72e1cd3688c8d97875
Maxime Leuger <mleuger@mediarithmics.com>
2018-03-22 19:28:19 +0100

    @ndussart improvements

--- app/react/src/containers/Campaigns/Goal/Edit/GoalFormService.ts
+++ app/react/src/containers/Campaigns/Goal/Edit/GoalFormService.ts
@@ -10,7 +10,6 @@ import {
   INITIAL_GOAL_FORM_DATA,
   isAttributionSelectionResource,
   isAttributionModelFormData,
-  TriggerMode,
 } from './domain';
 import { GoalResource } from '../../../../models/goal';
 import GoalService from '../../../../services/GoalService';
@@ -18,69 +17,47 @@ import AttributionModelFormService from '../../../Settings/CampaignSettings/Attr
 import queryService from '../../../../services/QueryService';
 
 const GoalFormService = {
-  // loadGoal(goalId: string): Promise<GoalFormData> {
-  //   return Promise.all([
-  //     GoalService.getGoal(goalId).then(res => res.data),
-  //     getLookbackWindow(goalId),
-  //   ]).then(([goal, lookbackWindow]) => {
-  //     return {
-  //       goal,
-  //       lookbackWindow,
-  //     };
-  //   });
-  // },
-
   loadGoalData(goalId: string, datamartId: string): Promise<NewGoalFormData> {
-    return GoalService.getGoal(goalId)
-      .then(resp => resp.data)
-      .then(formData => {
-        return GoalService.getAttributionModels(goalId)
-          .then(res => res.data)
-          .then(attributionModelList => {
-            const goalFormData: NewGoalFormData = {
-              goal: {},
-              attributionModels: [],
-              triggerMode: 'QUERY',
-            };
-            goalFormData.goal = formData;
-            goalFormData.attributionModels = attributionModelList.map(
-              attributionModel =>
-                createFieldArrayModelWithMeta(attributionModel, {
-                  name: attributionModel.attribution_model_name,
-                  group_id: attributionModel.group_id,
-                  artifact_id: attributionModel.artifact_id,
-                  default: attributionModel.default,
-                }),
-            );
-            const QueryContainer = (window as any).angular
-              .element(document.body)
-              .injector()
-              .get('core/datamart/queries/QueryContainer');
-            if (formData.new_query_id) {
-              return queryService
-                .getQuery(datamartId, formData.new_query_id)
-                .then(r => r.data)
-                .then(res => {
-                  const defQuery = new QueryContainer(datamartId, res.id);
-                  defQuery.load();
-                  const triggerMode: TriggerMode = 'QUERY';
-                  goalFormData.queryContainer = defQuery;
-                  goalFormData.triggerMode = triggerMode;
-                  return goalFormData;
-                });
-            } else {
-              const triggerMode: TriggerMode = 'PIXEL';
-              return Promise.resolve({
-                ...goalFormData,
-                queryContainer: new QueryContainer(datamartId),
-                triggerMode: triggerMode,
+    return Promise.all([
+      GoalService.getGoal(goalId),
+      GoalService.getAttributionModels(goalId),
+    ]).then(([goalRes, attribModelRes]) => {
+      const goalFormData: NewGoalFormData = {
+        goal: goalRes.data,
+        triggerMode: 'QUERY',
+        attributionModels: attribModelRes.data.map(attributionModel =>
+          createFieldArrayModelWithMeta(attributionModel, {
+            name: attributionModel.attribution_model_name,
+            group_id: attributionModel.group_id,
+            artifact_id: attributionModel.artifact_id,
+            default: attributionModel.default,
+          }),
+        ),
+      };
+      const QueryContainer = (window as any).angular
+        .element(document.body)
+        .injector()
+        .get('core/datamart/queries/QueryContainer');
+      if (goalRes.data.new_query_id) {
+        return queryService
+          .getQuery(datamartId, goalRes.data.new_query_id)
+          .then(r => r.data)
+          .then(res => {
+            return new QueryContainer(datamartId, res.id)
+              .load()
+              .then((queryContainer: any) => {
+                goalFormData.queryContainer = queryContainer;
+                return goalFormData;
               });
-            }
-          })
-          .then((resp: NewGoalFormData) => {
-            return resp;
           });
-      });
+      } else {
+        return {
+          ...goalFormData,
+          queryContainer: new QueryContainer(datamartId),
+          triggerMode: 'PIXEL',
+        };
+      }
+    });
   },
 
   saveGoal(
@@ -119,12 +96,6 @@ const GoalFormService = {
 
       return createOrUpdateGoalPromise.then(resp => {
         const goalResourceId = resp.data.id;
-        // if (goalFormData.lookbackWindow) {
-        //   return persistLookbackWindow(
-        //     organisationId,
-        //     goalResource.id,
-        //     goalFormData.lookbackWindow,
-        //   ).then(() => goalResource);
         const tasks: Task[] = [];
         tasks.push(
           ...getAttributionModelTasks(
@@ -142,56 +113,6 @@ const GoalFormService = {
 };
 
 export default GoalFormService;
-
-// function getLookbackWindow(
-//   goalId: string,
-// ): Promise<LookbackWindow | undefined> {
-//   const noLookbackWindow = undefined;
-//   return GoalService.getAttributionModels(goalId).then(res => {
-//     const lookbackWindowFound = res.data.find(
-//       ats => ats.artifact_id === LookbackWindowArtifactId && !!ats.default,
-//     );
-//     if (!lookbackWindowFound) {
-//       return noLookbackWindow;
-//     }
-//     return AttributionModelService.getAttributionModelProperties(
-//       lookbackWindowFound.attribution_model_id,
-//     ).then(propsRes => {
-//       const postViewProp = propsRes.data.find(
-//         prop => prop.technical_name === 'post_view',
-//       );
-//       const postClickProp = propsRes.data.find(
-//         prop => prop.technical_name === 'post_click',
-//       );
-//       if (!postViewProp && !postClickProp) return noLookbackWindow;
-//       return {
-//         postView: (postViewProp as IntPropertyResource).value.value,
-//         postClick: (postClickProp as IntPropertyResource).value.value,
-//       };
-//     });
-//   });
-// }
-
-// function persistLookbackWindow(
-//   organisationId: string,
-//   goalId: string,
-//   lookbackWindow: LookbackWindow,
-// ): Promise<AttributionSelectionResource> {
-//   return getOrCreateLookbackWindowAttributionModel(
-//     organisationId,
-//     lookbackWindow,
-//   ).then(lpAttributionModel => {
-//     return GoalService.createAttributionModel(goalId, {
-//       attribution_model_id: lpAttributionModel.id,
-//       attribution_type: 'WITH_PROCESSOR',
-//       default: true,
-//     }).then(res => res.data);
-//   });
-// }
-
-// function hasId<T extends { id: string }, Y>(resource: T | Y): resource is T {
-//   return (resource as T).id !== undefined;
-// }
 
 function getAttributionModelTasks(
   organisationId: string,
@@ -266,46 +187,3 @@ function getAttributionModelTasks(
   });
   return tasks;
 }
-
-// function getOrCreateLookbackWindowAttributionModel(
-//   organisationId: string,
-//   lookbackWindow: LookbackWindow,
-// ): Promise<AttributionModelResource> {
-//   const predefinedAttributionModelName = `PV${lookbackWindow.postView}PC${
-//     lookbackWindow.postClick
-//   }`;
-//   return AttributionModelService.getAttributionModels(organisationId).then(
-//     resp => {
-//       const found = resp.data.find(
-//         model =>
-//           model.name === predefinedAttributionModelName &&
-//           model.artifact_id === LookbackWindowArtifactId,
-//       );
-
-//       if (!found) {
-//         return AttributionModelService.createAttributionModel(organisationId, {
-//           artifact_id: LookbackWindowArtifactId,
-//           group_id: 'com.mediarithmics.attribution',
-//           mode: 'DISCOVERY',
-//           name: predefinedAttributionModelName,
-//         }).then(res => {
-//           const newLookbackWindowAttributionModel = res.data;
-//           return Promise.all([
-//             AttributionModelService.updateAttributionProperty(
-//               newLookbackWindowAttributionModel.id,
-//               'post_view',
-//               lookbackWindow.postView,
-//             ),
-//             AttributionModelService.updateAttributionProperty(
-//               newLookbackWindowAttributionModel.id,
-//               'post_click',
-//               lookbackWindow.postClick,
-//             ),
-//           ]).then(() => newLookbackWindowAttributionModel);
-//         });
-//       }
-
-//       return found;
-//     },
-//   );
-// }
