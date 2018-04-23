8e6fdc6a67ff9dc3928ed100b3dd6dcf87ba498f
Maxime Leuger <mleuger@mediarithmics.com>
2018-04-23 14:10:43 +0200

    new country list + France by default

--- app/react/src/containers/Campaigns/Display/Edit/messages.ts
+++ app/react/src/containers/Campaigns/Display/Edit/messages.ts
@@ -466,10 +466,6 @@ export default defineMessages({
     id: 'adGroupEditor.section.location.option2',
     defaultMessage: 'Exclude',
   },
-  contentSectionLocationOptionCountryPlaceholder: {
-    id: 'adGroupEditor.section.location.option.country.placeholder',
-    defaultMessage: 'Select country',
-  },
   contentSectionLocationModal1: {
     id: 'adGroupEditor.section.location.modal1',
     defaultMessage:
