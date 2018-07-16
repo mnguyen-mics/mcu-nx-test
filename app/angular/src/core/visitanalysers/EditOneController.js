a2556d7e4f0f964f997661f504fce7ec614ed5ce
Nicolas Dussart <ndussart@mediarithmics.com>
2018-07-16 16:34:19 +0200

    Fix undefined isEdit

--- app/angular/src/core/visitanalysers/EditOneController.js
+++ app/angular/src/core/visitanalysers/EditOneController.js
@@ -5,16 +5,13 @@ define(['./module'], function (module) {
   module.controller('core/visitanalysers/EditOneController', [
     '$scope', '$log', 'Restangular', 'core/common/auth/Session', 'lodash', '$stateParams', '$location', 'core/configuration', 'ngTableParams', '$window', 'core/common/auth/AuthenticationService', "core/common/WaitingService", "core/common/ErrorService", "core/bidOptimizer/PropertyContainer", "$q",
     function($scope, $log, Restangular, Session, _, $stateParams, $location, configuration, NgTableParams, $window, AuthenticationService, waitingService, errorService, PropertyContainer, $q) {
-      $scope.isEdit = $stateParams.id ? true : false;
       var activityAnalyserId = $stateParams.id;
-      var type = $stateParams.type;
-      if (isEdit) {
+      if (activityAnalyserId) {
         $location.path(Session.getV2WorkspacePrefixUrl() + '/settings/datamart/visit_analyzers/' + activityAnalyserId + '/edit');
       } else {
         $location.path(Session.getV2WorkspacePrefixUrl() + '/settings/datamart/visit_analyzers/create');
       }
-      
-     
+    
     }
   ]);
 });
