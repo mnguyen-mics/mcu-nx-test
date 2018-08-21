303ea7b1021919a5252f15baceca5bc474f54347
zobione <esanchez@mediarithmics.com>
2018-08-21 17:10:27 +0200

    Remove sub rendering functions from render and put it as a component

--- app/react/src/containers/QueryTool/JSONOTQL/Diagram/FieldNode/FieldNodeWidget.tsx
+++ app/react/src/containers/QueryTool/JSONOTQL/Diagram/FieldNode/FieldNodeWidget.tsx
@@ -144,174 +144,180 @@ class FieldNodeWidget extends React.Component<Props, State> {
     );
   };
 
-  render() {
-    const { node, treeNodeOperations, connectDropTarget, isDragging } = this.props;
+  renderEditNode = () => {
+    const {
+      node,
+      treeNodeOperations
+    } = this.props;
 
-    const onHover = (type: 'enter' | 'leave') => () =>
-      this.setState({ hover: type === 'enter' ? true : false });
+    const onSubmit = (val: FieldNodeFormDataValues) => {
+      treeNodeOperations.updateNode(node.treeNodePath, val.fieldNodeForm);
+      this.editNode(false);
+    };
+    const closeEdit = () => this.editNode(false);
+    const onMouseOver = () => this.props.lockGlobalInteraction(true);
+    const onMouseLeave = () => this.props.lockGlobalInteraction(false)
+
+    return (
+      <div
+        id={this.id}
+        style={{
+          backgroundColor: '#ffffff',
+          borderStyle: 'solid',
+          color: node.getColor(),
+          borderColor: node.getColor(),
+          zIndex: 999999,
+          padding: 10,
+          borderRadius: 4,
+          width: EDIT_FIELD_SIZE,
+        }}
+        className="mcs-form-container no-padding"
+        onMouseOver={onMouseOver}
+        onMouseLeave={onMouseLeave}
+      >
+        <div
+          style={{
+            width: 0,
+            height: 0,
+            borderLeft: `${ARROW_SIZE}px solid transparent`,
+            borderRight: `${ARROW_SIZE}px solid transparent`,
+            borderBottom: `${ARROW_SIZE}px solid ${node.getColor()}`,
+            transform: 'rotate(-90deg)',
+            position: 'absolute',
+            left: -ARROW_SIZE - (ARROW_SIZE / 2) + 2,
+            top: (node.getSize().height / 2) - (ARROW_SIZE / 2)
+          }}
+        />
+        <div
+          style={{
+            position: 'absolute',
+            top: 7,
+            right: 0
+          }}
+        >
+          <ButtonStyleless onClick={closeEdit}><McsIcon type="close" /></ButtonStyleless>
+        </div>
+        <FieldNodeFormWrapper
+          breadCrumbPaths={[]}
+          objectType={node.objectTypeInfo}
+          objectTypes={this.props.objectTypes}
+          initialValues={{ fieldNodeForm: node.fieldNode }}
+          onSubmit={onSubmit}
+          idToAttachDropDowns={this.wrapperId}
+        />
+        <WindowBodyPortal>
+          <div ref={this.setSelectRef} id={this.wrapperId} />
+        </WindowBodyPortal>
+      </div>
+    );
+  };
+
+  renderedStandardNode = (): JSX.Element => {
+    const {
+      node,
+    } = this.props;
+  
+    const onHover = (type: 'enter' | 'leave') => () => this.setState({ hover: type === 'enter' ? true : false });
+
     const onFocus = () => {
       this.props.lockGlobalInteraction(!this.state.focus);
       this.setPosition(document.getElementById(this.id) as HTMLDivElement);
       this.setState({ focus: !this.state.focus });
     };
 
-    const onSubmit = (val: FieldNodeFormDataValues) => {
-      treeNodeOperations.updateNode(node.treeNodePath, val.fieldNodeForm);
-      this.editNode(false);
-    };
-
     const zoomRatio = this.props.diagramEngine.getDiagramModel().zoom / 100;
 
     const triggerEdit = () => this.editNode(true);
-    const closeEdit = () => this.editNode(false);
 
-    const onMouseOver = () => this.props.lockGlobalInteraction(true);
-    const onMouseLeave = () => this.props.lockGlobalInteraction(false)
-    
-
-    const renderEditNode = () => {
-      return (
-        <div
-          id={this.id}
-          style={{
-            backgroundColor: '#ffffff',
-            borderStyle: 'solid',
-            color: node.getColor(),
-            borderColor: node.getColor(),
-            zIndex: 999999,
-            padding: 10,
-            borderRadius: 4,
-            width: EDIT_FIELD_SIZE,
-          }}
-          className="mcs-form-container no-padding"
-          onMouseOver={onMouseOver}
-          onMouseLeave={onMouseLeave}
-        >
-          <div
-            style={{
-              width: 0,
-              height: 0,
-              borderLeft: `${ARROW_SIZE}px solid transparent`,
-              borderRight: `${ARROW_SIZE}px solid transparent`,
-              borderBottom: `${ARROW_SIZE}px solid ${node.getColor()}`,
-              transform: 'rotate(-90deg)',
-              position: 'absolute',
-              left: -ARROW_SIZE - (ARROW_SIZE / 2) + 2,
-              top: (node.getSize().height / 2) - (ARROW_SIZE / 2)
-            }}
-          />
-          <div
-            style={{
-              position: 'absolute',
-              top: 7,
-              right: 0
-            }}
-          >
-            <ButtonStyleless onClick={closeEdit}><McsIcon type="close" /></ButtonStyleless>
-          </div>
-          <FieldNodeFormWrapper
-            breadCrumbPaths={[]}
-            objectType={node.objectTypeInfo}
-            objectTypes={this.props.objectTypes}
-            initialValues={{ fieldNodeForm: node.fieldNode }}
-            onSubmit={onSubmit}
-            idToAttachDropDowns={this.wrapperId}
-          />
-          <WindowBodyPortal>
-            <div ref={this.setSelectRef} id={this.wrapperId} />
-          </WindowBodyPortal>
+    return (
+      <div
+        id={this.id}
+        className="field-node"
+        onClick={onFocus}
+        style={{
+          ...node.getSize(),
+          backgroundColor: '#ffffff',
+          borderStyle: 'solid',
+          color: node.getColor(),
+          borderColor: node.getColor(),
+        }}
+        onMouseEnter={onHover('enter')}
+        onMouseLeave={onHover('leave')}
+      >
+        <div className="field">
+          <FieldNodeComparisonRenderer node={node} />
         </div>
-      );
-    };
+        <FourAnchorPortWidget node={node} />
+        {this.state.focus && (
+          <WindowBodyPortal>
+            <div className="query-builder full-screen">
+              <div
+                onClick={onFocus}
+                style={{
+                  position: 'fixed',
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                  backgroundColor: 'black',
+                  zIndex: 1000,
+                  opacity: 0.6,
+                }}
+              />
+              <span
+                className="object-node no-hover"
+                style={{
+                  ...node.getSize(),
+                  backgroundColor: '#ffffff',
+                  borderStyle: 'solid',
+                  color: node.getColor(),
+                  borderColor: node.getColor(),
+                  top:
+                    this.top - node.getSize().height * ((1 - zoomRatio) / 2),
+                  left:
+                    this.left - node.getSize().width * ((1 - zoomRatio) / 2),
 
-    
-
-    const renderedStandardNode = (): JSX.Element => {
-      return (
-        <div
-          id={this.id}
-          className="field-node"
-          onClick={onFocus}
-          style={{
-            ...node.getSize(),
-            backgroundColor: '#ffffff',
-            borderStyle: 'solid',
-            color: node.getColor(),
-            borderColor: node.getColor(),
-          }}
-          onMouseEnter={onHover('enter')}
-          onMouseLeave={onHover('leave')}
-        >
-          <div className="field">
-            <FieldNodeComparisonRenderer node={node} />
-          </div>
-          <FourAnchorPortWidget node={node} />
-          {this.state.focus && (
-            <WindowBodyPortal>
-              <div className="query-builder full-screen">
-                <div
-                  onClick={onFocus}
-                  style={{
-                    position: 'fixed',
-                    top: 0,
-                    left: 0,
-                    right: 0,
-                    bottom: 0,
-                    backgroundColor: 'black',
-                    zIndex: 1000,
-                    opacity: 0.6,
-                  }}
-                />
-                <span
-                  className="object-node no-hover"
-                  style={{
-                    ...node.getSize(),
-                    backgroundColor: '#ffffff',
-                    borderStyle: 'solid',
-                    color: node.getColor(),
-                    borderColor: node.getColor(),
-                    top:
-                      this.top - node.getSize().height * ((1 - zoomRatio) / 2),
-                    left:
-                      this.left - node.getSize().width * ((1 - zoomRatio) / 2),
-
-                    position: 'absolute',
-                    zIndex: 1002,
-                    transform: `scale(${zoomRatio})`,
-                  }}
-                  onClick={onFocus}
-                >
-                  <div className="field">
-                    <FieldNodeComparisonRenderer node={node} />
-                  </div>
-                </span>
-                <div
-                  className="boolean-menu"
-                  style={{
-                    top: this.top,
-                    left: this.left + node.getSize().width * zoomRatio,
-                    zIndex: 1001,
-                  }}
-                >
-                  <div onClick={triggerEdit} className="boolean-menu-item">
-                    <FormattedMessage {...messages.edit} />
-                  </div>
-                  <div onClick={this.removeNode} className="boolean-menu-item">
-                    <FormattedMessage {...messages.remove} />
-                  </div>
+                  position: 'absolute',
+                  zIndex: 1002,
+                  transform: `scale(${zoomRatio})`,
+                }}
+                onClick={onFocus}
+              >
+                <div className="field">
+                  <FieldNodeComparisonRenderer node={node} />
+                </div>
+              </span>
+              <div
+                className="boolean-menu"
+                style={{
+                  top: this.top,
+                  left: this.left + node.getSize().width * zoomRatio,
+                  zIndex: 1001,
+                }}
+              >
+                <div onClick={triggerEdit} className="boolean-menu-item">
+                  <FormattedMessage {...messages.edit} />
+                </div>
+                <div onClick={this.removeNode} className="boolean-menu-item">
+                  <FormattedMessage {...messages.remove} />
                 </div>
               </div>
-            </WindowBodyPortal>
-          )}
-        </div>
-      );
-    };
+            </div>
+          </WindowBodyPortal>
+        )}
+      </div>
+    );
+  };
+
+
+  render() {
+    const { connectDropTarget, isDragging } = this.props;  
 
     const opacity = isDragging ? 0.3 : 1;
 
     const renderedFieldNode = (
       <div ref={this.setWrapperRef} style={{opacity}} >
-        {this.state.edit ? renderEditNode() : renderedStandardNode()}
+        {this.state.edit ? this.renderEditNode() : this.renderedStandardNode()}
       </div>
     );
 
