ea475e56e1774bcff8e1da44a7a0cc34421f4820
zobione <esanchez@mediarithmics.com>
2018-09-03 17:08:50 +0200

    remove plugin content
    - Remove plugin content from the AudienceFeedForm
    - Add Plugin Generic Content to AudiencefeedForm
    - Add Plugin Generic Services
    
    Issue noted with Audience Tag Ressource

---
+++ app/react/src/services/AudienceExternalFeedService.ts
@@ -0,0 +1,129 @@
+import ApiService, { DataListResponse, DataResponse } from './ApiService';
+import { AudienceExternalFeed } from '../models/Plugins';
+import PluginInstanceService from './PluginInstanceService';
+import PluginService from './PluginService';
+import { PluginLayout } from '../models/plugin/PluginLayout';
+import { PropertyResourceShape } from '../models/plugin';
+
+class AudienceExternalFeedService extends PluginInstanceService<AudienceExternalFeed> {
+  segmentId: string;
+  constructor(segmentId: string) {
+    super("audience_external_feeds")
+    this.segmentId = segmentId;
+  }
+
+  getAudienceExternalFeeds(
+    organisationId: string,
+    options: object = {},
+  ): Promise<DataListResponse<AudienceExternalFeed>> {
+    const endpoint = 'plugins';
+
+    const params = {
+      plugin_type: "AUDIENCE_SEGMENT_EXTERNAL_FEED",
+      organisation_id: organisationId,
+      ...options,
+    };
+
+    return ApiService.getRequest(endpoint, params);
+  };
+
+  deleteAudienceExternalFeed(
+    id: string,
+    options: object = {},
+  ): Promise<DataResponse<any>> {
+    const endpoint = `audience_segments/${this.segmentId}/external_feeds/${id}`;
+
+    return ApiService.deleteRequest(endpoint, options);
+  };
+
+  // START reimplementation of method
+
+  getInstanceById(id: string, options: object = {}): Promise<DataResponse<AudienceExternalFeed>> {
+    const endpoint = `audience_segments/${this.segmentId}/external_feeds/${id}`;
+
+    const params = {
+      ...options,
+    };
+    return ApiService.getRequest(endpoint, params);
+  }
+
+  getInstanceProperties(
+    id: string,
+    options: object = {},
+  ): Promise<DataListResponse<PropertyResourceShape>> {
+    const endpoint = `audience_segments/${this.segmentId}/external_feeds/${id}/properties`;
+
+    return ApiService.getRequest(endpoint, options);
+  }
+
+  updatePluginInstance(
+    id: string,
+    options: object = {},
+  ): Promise<DataResponse<AudienceExternalFeed>> {
+    const endpoint = `audience_segments/${this.segmentId}/external_feeds/${id}`;
+
+    const params = {
+      ...options,
+    };
+
+    return ApiService.putRequest(endpoint, params);
+  }
+
+  updatePluginInstanceProperty(
+    organisationId: string,
+    id: string,
+    technicalName: string,
+    params: object = {},
+  ): Promise<DataResponse<PropertyResourceShape> | void> {
+    const endpoint = `audience_segments/${this.segmentId}/external_feeds/${id}/properties/technical_name=${technicalName}`;
+    return PluginService.handleSaveOfProperties(
+      params,
+      organisationId,
+      this.entityPath,
+      id,
+      endpoint,
+    );
+  }
+
+  createPluginInstance(
+    organisationId: string,
+    options: object = {},
+  ): Promise<DataResponse<AudienceExternalFeed>> {
+    const endpoint = `audience_segments/${this.segmentId}/external_feeds?organisation_id=${organisationId}`;
+
+    const params = {
+      ...options,
+    };
+
+    return ApiService.postRequest(endpoint, params);
+  }
+
+
+  // STOP
+
+
+  // OLD WAY AND DUMB WAY TO DO IT, TO CHANGE
+  getAudienceExternalFeedProperties(id: string, options: object = {}) {
+    const endpoint = `audience_segments/${this.segmentId}/external_feeds/${id}/properties`;
+
+    return ApiService.getRequest(endpoint, options).then((res: any) => {
+      return { ...res.data, id };
+    });
+  };
+
+  getLocalizedPluginLayout(pInstanceId: string): Promise<PluginLayout | null> {
+    return this.getInstanceById(pInstanceId).then(res => {
+      const audienceTagFeed = res.data;
+      return PluginService.findPluginFromVersionId(audienceTagFeed.version_id).then(pluginResourceRes => {
+        const pluginResource = pluginResourceRes.data;
+        return PluginService.getLocalizedPluginLayout(
+          pluginResource.id,
+          audienceTagFeed.version_id
+        );
+      });      
+    });
+  }
+
+};
+
+export default AudienceExternalFeedService;
---
+++ app/react/src/services/AudienceTagFeedServices.ts
@@ -0,0 +1,133 @@
+import ApiService, { DataListResponse, DataResponse } from './ApiService';
+import { AudienceTagFeed } from '../models/Plugins';
+import PluginInstanceService from './PluginInstanceService';
+import PluginService from './PluginService';
+import { PluginLayout } from '../models/plugin/PluginLayout';
+import { PropertyResourceShape } from '../models/plugin';
+
+class AudienceTagFeedService extends PluginInstanceService<AudienceTagFeed> {
+  segmentId: string;
+  constructor(segmentId: string) {
+    super("audience_tag_feeds")
+    this.segmentId = segmentId;
+  }
+
+  getAudienceTagFeeds(
+    organisationId: string,
+    options: object = {},
+  ): Promise<DataListResponse<AudienceTagFeed>> {
+    const endpoint = 'plugins';
+
+    const params = {
+      plugin_type: "AUDIENCE_SEGMENT_TAG_FEED",
+      organisation_id: organisationId,
+      ...options,
+    };
+
+    return ApiService.getRequest(endpoint, params);
+  };
+
+  deleteAudienceTagFeed(
+    id: string,
+    options: object = {},
+  ): Promise<DataResponse<any>> {
+    const endpoint = `audience_segments/${this.segmentId}/tag_feeds/${id}`;
+
+    return ApiService.deleteRequest(endpoint, options);
+  };
+
+
+    // START reimplementation of method
+
+    getInstanceById(id: string, options: object = {}): Promise<DataResponse<AudienceTagFeed>> {
+      const endpoint = `audience_segments/${this.segmentId}/tag_feeds/${id}`;
+  
+      const params = {
+        ...options,
+      };
+      return ApiService.getRequest(endpoint, params);
+    }
+  
+    getInstanceProperties(
+      id: string,
+      options: object = {},
+    ): Promise<DataListResponse<PropertyResourceShape>> {
+      const endpoint = `audience_segments/${this.segmentId}/tag_feeds/${id}/properties`;
+  
+      return ApiService.getRequest(endpoint, options);
+    }
+  
+    updatePluginInstance(
+      id: string,
+      options: object = {},
+    ): Promise<DataResponse<AudienceTagFeed>> {
+      const endpoint = `audience_segments/${this.segmentId}/tag_feeds/${id}`;
+  
+      const params = {
+        ...options,
+      };
+  
+      return ApiService.putRequest(endpoint, params);
+    }
+  
+    updatePluginInstanceProperty(
+      organisationId: string,
+      id: string,
+      technicalName: string,
+      params: object = {},
+    ): Promise<DataResponse<PropertyResourceShape> | void> {
+      const endpoint = `audience_segments/${this.segmentId}/tag_feeds/${id}/properties/technical_name=${technicalName}`;
+      return PluginService.handleSaveOfProperties(
+        params,
+        organisationId,
+        this.entityPath,
+        id,
+        endpoint,
+      );
+    }
+  
+    createPluginInstance(
+      organisationId: string,
+      options: object = {},
+    ): Promise<DataResponse<AudienceTagFeed>> {
+      const endpoint = `audience_segments/${this.segmentId}/tag_feeds?organisation_id=${organisationId}`;
+  
+      const params = {
+        ...options,
+      };
+  
+      return ApiService.postRequest(endpoint, params);
+    }
+  
+  
+    // STOP
+
+  // OLD WAY AND DUMB WAY TO DO IT, TO CHANGE
+  getAudienceTagFeedProperties(id: string, options: object = {}) {
+    const endpoint = `audience_segments/${this.segmentId}/tag_feeds/${id}/properties`;
+
+    return ApiService.getRequest(endpoint, options).then((res: any) => {
+      return { ...res.data, id };
+    });
+  };
+
+  getLocalizedPluginLayout(pInstanceId: string): Promise<PluginLayout | null> {
+    return this.getInstanceById(pInstanceId).then(res => {
+      //
+      // to uncomment when version id is available on the instance
+      //
+      // const audienceTagFeed = res.data;
+      // return PluginService.findPluginFromVersionId(audienceTagFeed.version_id).then(pluginResourceRes => {
+      //   const pluginResource = pluginResourceRes.data;
+      //   return PluginService.getLocalizedPluginLayout(
+      //     pluginResource.id,
+      //     audienceTagFeed.version_id
+      //   );
+      // });
+      return null;      
+    });
+  }
+
+};
+
+export default AudienceTagFeedService;
