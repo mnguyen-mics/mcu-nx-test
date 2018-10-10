mnguyen-mics/mcu-nx-test

commit 688424b65c9545af5807c931e3011123a2c81c37

Improve ReactAngular init flow

app/react/src/containers/QueryTool/SelectorQL/AngularQueryToolWidget.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import ReactAngular from '../../ReactAngular/ReactAngular';
 import { QueryResource } from '../../../models/datamart/DatamartResource';
 import { FormattedMessage } from 'react-intl';
+import { Spin } from 'antd';
 
 const messageProps = {
   id: 'query-tool.widget.angular-query.undefined',
@@ -21,6 +22,7 @@ export interface QueryContainer {
 
 interface AngularQueryToolWidgetState {
   sessionInitialized: boolean,
+  queryContainerInitialized: boolean
 }
 
 declare global { namespace JSX { interface IntrinsicElements { "mcs-query-tool": any } } }
@@ -36,17 +38,33 @@ export default class AngularQueryToolWidget extends React.Component<AngularQuery
 
   constructor(props: AngularQueryToolWidgetProps) {
     super(props);
-    this.state = { sessionInitialized: false }
-    this.queryContainer = new this.AngularQueryContainer(this.props.datamartId)
+    this.state = { sessionInitialized: false, queryContainerInitialized: false }
   }
 
   componentDidMount() {
     this.props.setStateWithQueryContainer(this.queryContainer);
-    this.AngularSession.init(`o${this.props.organisationId}d${this.props.datamartId}`).then(() => this.setState({ sessionInitialized: true }))
+    this.AngularSession.init(`o${this.props.organisationId}d${this.props.datamartId}`)
+      .then(() => {
+        this.setState({ sessionInitialized: true })
+      })
+      .then(() => {
+        this.queryContainer = new this.AngularQueryContainer(this.props.datamartId)
+        this.setState({
+          queryContainerInitialized: true
+        })
+      })
+      .catch(() => this.setState({
+        sessionInitialized: true,
+        queryContainerInitialized: true
+      }))
   }
 
   render() {
-    return this.AngularQueryContainer ? (
+    if (!this.state.sessionInitialized) {
+      return <Spin />
+    }
+
+    return this.state.queryContainerInitialized ? (
       <ReactAngularJS
         scope={{
           container: this.queryContainer,

app/react/src/containers/Automations/Edit/sections/AutomationFormSection.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import ReactAngular from '../../../ReactAngular/ReactAngular';
 import { FormattedMessage } from 'react-intl';
+import { Spin } from 'antd';
 
 const messageProps = {
   id: 'automation.form.scenario-container.undefined',
@@ -14,7 +15,7 @@ export interface AngularWidgetProps {
 }
 
 interface AngularWidgetState {
-  QueryContainer: any;
+  sessionInitialized: boolean
 }
 
 declare global {
@@ -31,17 +32,30 @@ export default class AngularWidget extends React.Component<
   AngularWidgetProps,
   AngularWidgetState
 > {
+
+  AngularSession = (window as any).angular.element(document.body).injector().get('core/common/auth/Session')
+
   constructor(props: AngularWidgetProps) {
     super(props);
-    
-    (window as any).angular
-      .element(document.body)
-      .injector()
-      .get('core/common/auth/Session')
-      .init(`o${props.organisationId}d${props.datamartId}`);
+    this.state  = {
+      sessionInitialized: false,
+    }
+
+  }
+  componentDidMount() {
+    this.AngularSession.init(`o&{props.organisationId}d${this.props.datamartId}`)
+    .then(() => {
+      this.setState({ sessionInitialized: true })
+    })
+    .catch(() => this.setState({ sessionInitialized: true }))
   }
 
+
   render() {
+    if (!this.state.sessionInitialized) {
+      return <Spin />
+    }
+
     return this.props.scenarioContainer ? (
       <ReactAngularJS
         scope={{

app/react/src/containers/ReactAngular/ReactAngular.js
@@ -5,6 +5,9 @@ import PropTypes from 'prop-types';
 // inpired by https://www.npmjs.com/package/react-angular
 
 class ReactAngular extends Component {
+
+  AngularSession = window.angular.element(window.document.body).injector().get('core/common/auth/Session');
+
   constructor(props) {
     super(props);
     if (window.angular) {
@@ -35,6 +38,8 @@ class ReactAngular extends Component {
     $compile(this.$element)(this.$scope);
     this.$element.data('$scope', this.$scope);
     $rootScope.$evalAsync();
+
+    this.AngularSession.init(`o${this.props.scope.organisationId}d${this.props.scope.datamartId}`).then(() => this.setState({ sessionInitialized: true }));
   }
 
 

commit 281141c006c65b11d5b1a5416bacd49212ac3a37

MICS-396 Fix SaveAs hanging loader
Root Cause Analysis:

Its a regression caused by commit 688424b65c9545af5807c931e3011123a2c81c37
In some cases, setStateWithQueryContainer is called queryContainer=null

This commit call this function after the new AngularQueryContainer when
AngularSession has been initialized.

Moreover, sometimes angular's injector is null when using in class
instance field. Since the AngularQueryToolWidget component should be
mounted when navigator app has finished initialization, injector shouldn't be null

app/react/src/containers/Audience/SegmentBuilder/SegmentBuilderPage.tsx
@@ -119,7 +119,7 @@ class SegmentBuilderPage extends React.Component<Props> {
       datamartId: string,
     ) => {
       const saveAsUserQuery = (segmentFormData: NewUserQuerySimpleFormData) => {
-        if (!query) return Promise.resolve();
+        if (!query) return Promise.reject(new Error("angular query container isn't loaded correctly"));
         return query.saveOrUpdate().then(queryResource => {
           const { name, technical_name, persisted } = segmentFormData;
           const userQuerySegment: Partial<UserQuerySegment> = {
@@ -144,7 +144,7 @@ class SegmentBuilderPage extends React.Component<Props> {
         });
       };
       const saveAsExport = (exportFormData: NewExportSimpleFormData) => {
-        if (!query) return Promise.resolve();
+        if (!query) return Promise.reject(new Error("angular query container isn't loaded correctly"));
         return query.saveOrUpdate().then(queryResource => {
           return ExportService.createExport(match.params.organisationId, {
             name: exportFormData.name,

app/react/src/containers/Datastudio/QueryTool/QueryToolPage.tsx
@@ -77,7 +77,7 @@ class QueryToolPage extends React.Component<Props> {
       datamartId: string,
     ) => {
       const saveAsUserQuery = (segmentFormData: NewUserQuerySimpleFormData) => {
-        if (!query) return Promise.resolve();
+        if (!query) return Promise.reject(new Error("angular query container isn't loaded correctly"));
         return query.saveOrUpdate().then(queryResource => {
           const { name, technical_name, persisted } = segmentFormData;
           const userQuerySegment: Partial<UserQuerySegment> = {
@@ -102,7 +102,7 @@ class QueryToolPage extends React.Component<Props> {
         });
       };
       const saveAsExport = (exportFormData: NewExportSimpleFormData) => {
-        if (!query) return Promise.resolve();
+        if (!query) return Promise.reject(new Error("angular query container isn't loaded correctly"));
         return query.saveOrUpdate().then(queryResource => {
           return ExportService.createExport(match.params.organisationId, {
             name: exportFormData.name,

app/react/src/containers/QueryTool/SelectorQL/AngularQueryToolWidget.tsx
@@ -32,8 +32,8 @@ const ReactAngularJS = ReactAngular as any;
 
 export default class AngularQueryToolWidget extends React.Component<AngularQueryToolWidgetProps, AngularQueryToolWidgetState> {
 
-  AngularQueryContainer = (window as any).angular.element(document.body).injector().get('core/datamart/queries/QueryContainer');
-  AngularSession = (window as any).angular.element(document.body).injector().get('core/common/auth/Session');
+  AngularQueryContainer: any;
+  AngularSession: any;
   queryContainer: QueryContainer
 
   constructor(props: AngularQueryToolWidgetProps) {
@@ -41,14 +41,16 @@ export default class AngularQueryToolWidget extends React.Component<AngularQuery
     this.state = { sessionInitialized: false, queryContainerInitialized: false }
   }
 
-  componentDidMount() {
-    this.props.setStateWithQueryContainer(this.queryContainer);
+  componentDidMount() {        
+    this.AngularQueryContainer = (window as any).angular.element(document.body).injector().get('core/datamart/queries/QueryContainer');
+    this.AngularSession = (window as any).angular.element(document.body).injector().get('core/common/auth/Session');
     this.AngularSession.init(`o${this.props.organisationId}d${this.props.datamartId}`)
       .then(() => {
         this.setState({ sessionInitialized: true })
       })
       .then(() => {
         this.queryContainer = new this.AngularQueryContainer(this.props.datamartId)
+        this.props.setStateWithQueryContainer(this.queryContainer);
         this.setState({
           queryContainerInitialized: true
         })