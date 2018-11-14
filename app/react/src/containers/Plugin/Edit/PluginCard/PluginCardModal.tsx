25322215e8260c41c3c384a994a5f2e579da7f8f
zobione <esanchez@mediarithmics.com>
2018-11-14 17:02:02 +0100

    MICS-488 Fix extension of type
    Fix extension of type

--- app/react/src/containers/Plugin/Edit/PluginCard/PluginCardModal.tsx
+++ app/react/src/containers/Plugin/Edit/PluginCard/PluginCardModal.tsx
@@ -1,23 +1,10 @@
 import * as React from 'react';
 import StandardModal from '../../../../components/BlurredModal/StandardModal';
-import PluginCardModalContent from './PluginCardModalContent';
+import PluginCardModalContent, { PluginCardModalContentProps } from './PluginCardModalContent';
 import { LayoutablePlugin } from '../../../../models/Plugins';
-import { PropertyResourceShape } from '../../../../models/plugin';
-import { PluginLayout } from '../../../../models/plugin/PluginLayout';
 
-export interface PluginCardModalProps<T> {
+export interface PluginCardModalProps<T> extends PluginCardModalContentProps<T> {
   opened: boolean;
-  organisationId: string;
-  onClose: () => any;
-  plugin: T;
-  save: (pluginValue: any, propertiesValue: PropertyResourceShape[]) => void;
-  pluginProperties: PropertyResourceShape[];
-  disableFields: boolean;
-  pluginLayout: PluginLayout;
-  isLoading: boolean;
-  pluginVersionId: string;
-  initialValues?: any;
-  editionMode: boolean;
 }
 
 export default class PluginCardModal<T extends LayoutablePlugin> extends React.Component<PluginCardModalProps<T>, any> {
