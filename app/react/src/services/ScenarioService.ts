4fd5b4b084ceeab2f43fdebe6c2a4dc921f9381c
Maxime Leuger <mleuger@mediarithmics.com>
2018-12-20 18:02:15 +0100

    MICS-LAB make scenarioService prettier
    this commits makes scenarioService prettier

--- app/react/src/services/ScenarioService.ts
+++ app/react/src/services/ScenarioService.ts
@@ -1,7 +1,15 @@
 import ApiService, { DataListResponse, DataResponse } from './ApiService';
-import { AutomationResource, AutomationCreateResource, AutomationStatus } from '../models/automations/automations';
+import {
+  AutomationResource,
+  AutomationCreateResource,
+  AutomationStatus,
+} from '../models/automations/automations';
 import { PaginatedApiParam } from '../utils/ApiHelper';
-import { PAGINATION_SEARCH_SETTINGS, FILTERS_SEARCH_SETTINGS, KEYWORD_SEARCH_SETTINGS } from '../utils/LocationSearchHelper';
+import {
+  PAGINATION_SEARCH_SETTINGS,
+  FILTERS_SEARCH_SETTINGS,
+  KEYWORD_SEARCH_SETTINGS,
+} from '../utils/LocationSearchHelper';
 
 export interface GetAutomationsOptions extends PaginatedApiParam {
   organisation_id?: string;
@@ -19,7 +27,10 @@ export const SCENARIOS_SEARCH_SETTINGS = [
 ];
 
 const scenariosService = {
-  getScenarios(organisationId: string, options: object = {}): Promise<DataListResponse<AutomationResource>> {
+  getScenarios(
+    organisationId: string,
+    options: object = {},
+  ): Promise<DataListResponse<AutomationResource>> {
     const endpoint = 'scenarios';
     const params = {
       organisation_id: organisationId,
@@ -27,18 +38,27 @@ const scenariosService = {
     };
     return ApiService.getRequest(endpoint, params);
   },
-  getScenario(scenarioId: string, options: object = {}): Promise<DataResponse<AutomationResource>> {
+  getScenario(
+    scenarioId: string,
+    options: object = {},
+  ): Promise<DataResponse<AutomationResource>> {
     const endpoint = `scenarios/${scenarioId}`;
     return ApiService.getRequest(endpoint, options);
   },
-  createScenario(organisationId: string, scenario: AutomationCreateResource): Promise<DataResponse<AutomationResource>> {
+  createScenario(
+    organisationId: string,
+    scenario: AutomationCreateResource,
+  ): Promise<DataResponse<AutomationResource>> {
     const endpoint = `scenarios?organisation_id=${organisationId}`;
     return ApiService.postRequest(endpoint, scenario);
   },
-  updateScenario(scenarioId: string, scenario: AutomationResource): Promise<DataResponse<AutomationResource>> {
+  updateScenario(
+    scenarioId: string,
+    scenario: AutomationResource,
+  ): Promise<DataResponse<AutomationResource>> {
     const endpoint = `scenarios/${scenarioId}`;
     return ApiService.putRequest(endpoint, scenario);
-  }
-}
+  },
+};
 
 export default scenariosService;
