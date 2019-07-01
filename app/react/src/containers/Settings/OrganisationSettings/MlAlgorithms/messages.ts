3d11a0d747b8a3d5d486426c946275cc2cb2a485
Alexis Durieux <adurieux@mediarithmics.com>
2019-07-01 13:23:00 +0200

    MICS-DEMO Use TableView instead of ItemList
    Use TableView instead of ItemList which is more appropriate for our use
    case.

--- app/react/src/containers/Settings/OrganisationSettings/MlAlgorithms/messages.ts
+++ app/react/src/containers/Settings/OrganisationSettings/MlAlgorithms/messages.ts
@@ -2,25 +2,42 @@ import { defineMessages } from 'react-intl';
 
 const messages = defineMessages({
   id: {
-    id: 'settings.organisation.mlagorithms.id',
+    id: 'settings.organisation.ml-algorithms.id',
     defaultMessage: 'Id',
   },
   name: {
-    id: 'settings.organisation.mlagorithms.name',
+    id: 'settings.organisation.ml-algorithms.name',
     defaultMessage: 'Name',
   },
   description: {
-    id: 'settings.organisation.mlagorithms.description',
+    id: 'settings.organisation.ml-algorithms.description',
     defaultMessage: 'Description',
   },
+  lastUpdatedDate: {
+    id: 'settings.organisation.ml-algorithms.last-updated-date',
+    defaultMessage: 'Last Updated Date',
+  },
   emptyMlAlgorithms: {
-    id: 'settings.organisation.mlagorithms.empty-ml-algorithms',
+    id: 'settings.organisation.ml-algorithms.empty-ml-algorithms',
     defaultMessage: 'No ML Algorithms for organisation',
   },
   mlAlgorithms: {
-    id: 'settings.organisation.mlalgorithms.ml-algorithms',
+    id: 'settings.organisation.ml-algorithms.placeholder',
     defaultMessage: 'ML Algorithms'
+  },
+  edit: {
+    id: 'settings.organisation.ml-algorithms.edit',
+    defaultMessage: 'Edit'
+  },
+  downloadNotebook: {
+    id: 'settings.organisation.ml-algorithms.download-notebook',
+    defaultMessage: 'Download Notebook'
+  },
+  archive: {
+    id: 'settings.organisation.ml-algorithms.archive',
+    defaultMessage: 'Archive'
   }
+  
 });
 
 export default messages;
