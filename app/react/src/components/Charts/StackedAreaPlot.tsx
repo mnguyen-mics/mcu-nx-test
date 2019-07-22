2143dcbdc9358e7007cf37f2d8ab0090d19d9b87
zobione <esanchez@mediarithmics.com>
2019-07-22 11:00:42 +0200

    MICS-DEV Introduce highchart for Stacked Area Plot
    - Set up highchart
    - Implement Stacked Area Plot
    - Change the Segment Overview Plot to new Component

---
+++ app/react/src/components/Charts/StackedAreaPlot.tsx
@@ -0,0 +1,181 @@
+import * as React from 'react';
+import * as Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+import { injectIntl, FormattedMessage, InjectedIntlProps } from 'react-intl';
+import { compose } from 'recompose';
+import cuid from 'cuid';
+import moment from 'moment';
+import { AREA_OPACITY, generateXAxisGridLine, generateYAxisGridLine, generateTooltip } from './domain';
+
+export interface StackedAreaPlotProps {
+  dataset: Dataset;
+  options: ChartOptions;
+  style?: any;
+}
+
+type Dataset = Array<{ [key: string]: string | number | Date | undefined }>;
+
+interface ChartOptions {
+  colors: string[];
+  yKeys: yKey[];
+  xKey: string;
+  isDraggable?: boolean;
+  onDragEnd?: any;
+}
+
+type yKey = { key: string; message: FormattedMessage.MessageDescriptor };
+
+type Props = StackedAreaPlotProps & InjectedIntlProps;
+
+class StackedAreaPlot extends React.Component<Props, {}> {
+  id: string = cuid();
+
+  constructor(props: Props) {
+    super(props);
+    this.state = {};
+  }
+
+  formatSeries = (
+    dataset: Dataset,
+    xKey: string,
+    yKeys: yKey[],
+    colors: string[],
+  ): Highcharts.SeriesOptionsType[] => {
+    const {
+      intl: { formatMessage },
+    } = this.props;
+    return yKeys.map((y, i) => {
+      return {
+        type: 'area' as any,
+        data: dataset.map(data => {
+          const yValue = data[y.key];
+          return [
+            this.formatDateToTs(data[xKey] as string),
+            yValue && typeof yValue === 'string' ? parseFloat(yValue) : yValue,
+          ];
+        }),
+        name: formatMessage(y.message),
+        color: colors[i],
+        fillOpacity: 0.5,
+        fillColor: {
+          linearGradient: {
+            x1: 0,
+            y1: 0,
+            x2: 0,
+            y2: 1,
+          },
+          stops: [
+            [
+              0,
+              (Highcharts as any)
+                .Color(colors[i])
+                .setOpacity(AREA_OPACITY)
+                .get('rgba'),
+            ],
+            [
+              1,
+              (Highcharts as any)
+                .Color(colors[i])
+                .setOpacity(0)
+                .get('rgba'),
+            ],
+          ],
+        },
+      };
+    });
+  };
+
+  formatDateToTs = (date: string) => {
+    return moment(date)
+      .seconds(0)
+      .hours(0)
+      .milliseconds(0)
+      .minutes(0)
+      .valueOf();
+  };
+
+  formatXAxis = (dataset: Dataset, xKey: string) => {
+    return dataset.map(d => this.formatDateToTs(d[xKey] as string));
+  };
+
+  generateMinValue = (dataset: Dataset, yKeys: yKey[],) => {
+    const values: number[] = [];
+    dataset.forEach(d => {
+      yKeys.forEach(y => {
+        values.push(d[y.key] as number);
+      })
+    })
+    return Math.min(...values);
+  }
+
+  render() {
+    const {
+      dataset,
+      options: { xKey, yKeys, colors },
+    } = this.props;
+
+    const options: Highcharts.Options = {
+      global: {
+        timezoneOffset: new Date().getTimezoneOffset(),
+      },
+      title: {
+        text: '',
+      },
+      colors: colors,
+      plotOptions: {
+        area: {
+          animation: true,
+          marker: {
+            radius: 4,
+            symbol: 'circle',
+          },
+          lineWidth: 1,
+          states: {
+            hover: {
+              lineWidth: 1,
+            },
+          },
+          threshold: null,
+        } as any,
+      },
+      xAxis: {
+        type: 'datetime',
+        dateTimeLabelFormats: {
+          month: '%e. %b' as any,
+          year: '%b' as any,
+        },
+        ...generateXAxisGridLine()
+      },
+      time: {
+        timezoneOffset: new Date().getTimezoneOffset(),
+        useUTC: true,
+      },
+      yAxis: {
+        title: {
+          text: null,
+        },
+        ...generateYAxisGridLine()
+      },
+      series: this.formatSeries(dataset, xKey, yKeys, colors),
+      credits: {
+        enabled: false,
+      },
+      tooltip: {
+        shared: true,
+        ...generateTooltip()
+      },
+    };
+
+    return (
+      <HighchartsReact
+        highcharts={Highcharts}
+        options={options}
+        style={{ width: '100%', height: 400 }}
+      />
+    );
+  }
+}
+
+export default compose<Props, StackedAreaPlotProps>(injectIntl)(
+  StackedAreaPlot,
+);
---
+++ app/react/src/components/Charts/domain.ts
@@ -0,0 +1,49 @@
+import * as Highcharts from "highcharts";
+
+export const GRAY_COLOR = "#8ca0b3";
+export const LINE_COLOR = (Highcharts as any).Color(GRAY_COLOR).setOpacity(0.4).get('rgba');
+export const CROSSHAIR_COLOR = (Highcharts as any).Color(GRAY_COLOR).setOpacity(0.6).get('rgba');
+export const AREA_OPACITY = 0.15
+
+export const generateXAxisGridLine = (): Partial<Highcharts.XAxisOptions> => {
+  return {
+    gridLineWidth: 1,
+    gridLineColor: LINE_COLOR,
+    gridLineDashStyle: 'ShortDash',
+    crosshair: {
+      color: CROSSHAIR_COLOR,
+      width: 1,
+      dashStyle: 'ShortDash',
+    },
+  }
+}
+
+export const generateYAxisGridLine = (): Partial<Highcharts.YAxisOptions> => {
+  return {
+    gridLineWidth: 1,
+    gridLineColor: LINE_COLOR,
+    gridLineDashStyle: 'ShortDash',
+  }
+}
+
+export const generateTooltip = (): Partial<Highcharts.TooltipOptions> => {
+  return {
+    crosshairs: true,
+    useHTML: true,
+    borderRadius: 2,
+    borderWidth: 1,
+    borderColor: "#e8e8e8",
+    padding: 15,
+    outside: false,
+    shadow: false,
+    hideDelay: 0,
+    headerFormat: `<span style="font-size: 12px; font-weight: bold; margin-bottom: 13px;">{point.key}</span><br/><br/>`,
+    pointFormat: `<span style="color:{point.color}; font-size: 20px; margin-right: 20px;">\u25CF</span> {series.name}: <b>{point.y}</b><br/>`
+  }
+}
+
+export const generateLegend = (): Partial<Highcharts.LegendOptions> => {
+  return {
+    
+  }
+}
