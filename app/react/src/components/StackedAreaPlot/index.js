f50bec3ef3897d51e78166c68289add850a37974
zobione <esanchez@mediarithmics.com>
2019-07-24 11:06:16 +0200

    MICS-DEV Remove Unused Components
    Remove Unused Components

--- app/react/src/components/StackedAreaPlot/index.js
+++ app/react/src/components/StackedAreaPlot/index.js
@@ -1,9 +1,5 @@
-import StackedAreaPlot from './StackedAreaPlot';
 import StackedLinePlot from './StackedLinePlot';
-import StackedAreaPlotDoubleAxis from './StackedAreaPlotDoubleAxis.tsx';
 
 export {
-  StackedAreaPlot,
   StackedLinePlot,
-  StackedAreaPlotDoubleAxis,
 };
