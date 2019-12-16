c9b59762f9a1bf4f33eb8c6890baa57a0e329ade
Alexis Durieux <adurieux@mediarithmics.com>
2019-12-16 17:15:16 +0100

    MICS-3177 Filter variables creation if key is empty
    We don't want to have variables with empty keys.

--- app/react/src/containers/Settings/DatamartSettings/MlAlgorithms/messages.ts
+++ app/react/src/containers/Settings/DatamartSettings/MlAlgorithms/messages.ts
@@ -129,7 +129,7 @@ const messages = defineMessages({
   mlVariableValuePlaceholder: {
     id: 'settings.organisation.ml-algorithms.ml-variable.placeholder.value',
     defaultMessage: 'Value',
-  },
+  }
 });
 
 export default messages;
