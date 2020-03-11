fcceca0f475b172b22e6084e36d69ebc4c0699f4
Igor EPIMAKHOV <iepimakhov@mediarithmics.com>
2020-03-11 10:32:16 +0100

    MICS-4074 add sessions by origin channel histogram
    * add a new config and place the chart to the page
    
    Signed-off-by: Igor EPIMAKHOV <iepimakhov@mediarithmics.com>

--- app/react/src/containers/Audience/DatamartUsersAnalytics/config/AnalyticsConfigJson.tsx
+++ app/react/src/containers/Audience/DatamartUsersAnalytics/config/AnalyticsConfigJson.tsx
@@ -653,6 +653,71 @@ export const acquisitionEngagementConfig = [
     ]
   },
   {
+    title: 'Sessions by Origin',
+    layout: {
+      'i': '1',
+      'h': 3,
+      'static': false,
+      'w': 6,
+      'x': 6,
+      'y': 3
+    },
+    charts: [
+      {
+        type: 'COLUMN',
+        options: {
+          title: undefined,
+          height: 300,
+          colors: chroma.scale(['#00a1df','#003056']).mode('lch').colors(5),
+          credits: {
+            enabled: false
+          },
+          chart: {
+            reflow: true
+          },
+          xAxis: {
+            ...generateXAxisGridLine(),
+            type: 'column'
+          },
+          time: { timezoneOffset: -60, useUTC: true },
+          yAxis: {
+            ...generateYAxisGridLine(),
+            title: {
+              text: null
+            }
+          },
+          legend: {
+            enabled: true,
+            verticalAlign: 'top',
+            itemMarginBottom: 6
+          },
+          tooltip: {
+            shared: true,
+            ...generateTooltip()
+          }
+        },
+        dimensions: ['origin_channel'],
+        metricName: 'sessions',
+        dimensionFilterClauses: {
+          'operator': 'OR',
+          'filters': [
+            {
+              'dimension_name': 'type',
+              'not': false,
+              'operator': 'IN_LIST',
+              'expressions': [
+                'SITE_VISIT',
+                'APP_VISIT'
+              ],
+              'case_sensitive': false
+            }
+          ]
+        },
+      }
+    ]
+  },
+
+  {
     title: 'Average Session duration by origin source',
     layout: {
       'i': '1',
