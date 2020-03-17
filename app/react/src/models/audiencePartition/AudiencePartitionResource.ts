007269f774ffa2df9b0df60638893a5d28b02e25
Maxime Leuger <mleuger@mediarithmics.com>
2020-03-17 17:31:58 +0100

    MICS-4383 fetch not archived partitions
    fetch not archived partitions in partitions page

--- app/react/src/models/audiencePartition/AudiencePartitionResource.ts
+++ app/react/src/models/audiencePartition/AudiencePartitionResource.ts
@@ -3,10 +3,10 @@ export type AudiencePartitionType = 'CLUSTERING' | 'RANDOM_SPLIT' | 'TREE_SPLIT'
 export type AudiencePartitionStatus = 'DRAFT' | 'PUBLISHED';
 
 export interface AudiencePartitionResource {
-	id?: string;
-	organisation_id?: string;
+	id: string;
+	organisation_id: string;
 	name?: string;
-	datamart_id?: string;
+	datamart_id: string;
 	part_count?: number;
 	clustering_model_data_file_uri?: string;
 	audience_partition_type?: AudiencePartitionType;
