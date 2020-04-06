6b5de99adbc7d4f543efe13d1d8aa8f0e015cf40
nguillomon <nguillomon@mediarithmics.com>
2020-04-06 15:15:48 +0200

    MICS-4592 Remove ChartTooltip, not used
    Remove ChartTooltips and css relaated because
    it's not used.

--- app/react/src/components/ChartTooltip/index.ts
+++
@@ -1,9 +0,0 @@
-import ChartTooltip from './ChartTooltip';
-import BasicTooltip from './BasicTooltip';
-import OverlapTooltip from './OverlapTooltip';
-
-export {
-  ChartTooltip,
-  BasicTooltip,
-  OverlapTooltip,
-};
