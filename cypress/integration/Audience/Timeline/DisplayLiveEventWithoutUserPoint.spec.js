62bbb1aaef1873d4beb10ac6540c746e8172a8df
Guillaume Cavelier <gcavelier@mediarithmics.com>
2020-04-22 12:27:39 +0200

    MICS-4557 Add test to check presence of LIVE event in timeline
    We can't see a LIVE event in the timeline if the
    user point related to a given activity is not
    created yet.
    
    This bug is a regression, it was previously fixed in december.
    Adding a Cypress test will prevent new regression.
    
    To test it, we make a call api to create an IN_SESSION
    activity with a new identifier. We then check if the
    activity is displayed on the timeline.

---
+++ cypress/integration/Audience/Timeline/DisplayLiveEventWithoutUserPoint.spec.js
@@ -0,0 +1,62 @@
+describe('A Live Event without user point related is correctly displayed on the timeline ', () => {
+  const second = 1000;
+
+  beforeEach(() => {
+    cy.viewport(1920, 1080)
+  })
+
+  before(() => {
+    // Login
+    cy.login()
+    cy.url({ timeout: 10 * second }).should("contain", Cypress.config().baseUrl +"/#/v2/o/1/campaigns/display").then(() => {
+      cy.request({
+        method:  "post", 
+        url: Cypress.env('apiDomain') + "/v1/datamarts/1162/user_activities?processing_pipeline=true&async=false",
+        body: {
+          "$type":"SITE_VISIT",
+          "$session_status":"IN_SESSION",
+          "$ttl":null,
+          "$email_hash":null,
+          "$origin": null,
+          "$location":null,
+          "$events":[
+            {
+            "$ts":1586964161517,
+            "$event_name":"test",
+            "$properties": {
+                "$url":"https://www.mediarithmics.com",
+                "$referrer":""
+              }
+            }
+          ],
+          "$error_analyzer_id":null,
+          "$user_agent_id":"vec:10",
+          "$ts":1586964161517,
+          "$session_duration":0,
+          "$topics":{},
+          "$site_id": "2583"
+        },
+        headers:  {
+          authorization: localStorage.getItem('access_token')
+        }
+      }).then((response) => {
+        expect(response.body).to.have.property('status', 'ok')
+        // Even if we got 200 response, we have to wait a bit before user-activity-processing process the activity
+        cy.wait(500);
+        cy.visit(Cypress.config().baseUrl + '/#/v2/o/504/audience/timeline/user_agent_id/vec:10?datamartId=1162')
+      })
+    })
+  })
+
+  it('Should display json source modal', () => {
+    cy.get('.ant-timeline-item').eq(1).find('.mcs-card-inner-action').first().as('view_json_source')
+    cy.get('.ant-timeline-item').eq(1).find('.mcs-card-title').first().should('have.text', 'Site: mediarithmics')
+    cy.get('@view_json_source').should('have.text', 'View JSON source').click()
+    cy.get('.ant-modal-content').should('be.visible') 
+    cy.get('.ant-modal-confirm-title').should('have.text', 'Activity JSON')
+    cy.get('.ant-modal-confirm-btns .ant-btn').should('have.text', 'Close').click()
+    cy.get('.ant-modal-content').should('not.be.visible') 
+  })
+
+})
+
