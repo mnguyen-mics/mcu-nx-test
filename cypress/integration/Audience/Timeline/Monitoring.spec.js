578393a8b2f711f145dd695aca02617c88095515
Oussama Assassi <oassassi@mediarithmics.com>
2020-05-28 14:41:51 +0200

    MICS-1596 fix Maxime and Guillaume feedbacks
    fix Maxime and Guillaume feedbacks

--- cypress/integration/Audience/Timeline/Monitoring.spec.js
+++ cypress/integration/Audience/Timeline/Monitoring.spec.js
@@ -5,16 +5,32 @@
 
 describe('Timeline', () => {
   const second = 1000;
+  const organisationName = "ncbewfydygvj";
+  const datamartName = "pjbatbxmg";
 
   before(() => {
     // Login
     cy.login()
+    
     cy.url({ timeout: 10 * second }).should("contain", Cypress.config().baseUrl +"/#/v2/o/1/campaigns/display");
-    cy.visit(Cypress.config().baseUrl + '/#/v2/o/1437/audience/timeline/user_account_id/user_id_0?datamartId=1518&compartmentId=1592')
+
+    cy.switchOrg(organisationName);
+
+    //Go to Segment menu
+    cy.contains("Audience").click();
+
+    cy.contains("Monitoring").click();
+
+    cy.contains(organisationName).click();
+
+    cy.contains('User Lookup').click();
+
+    cy.get('.ant-input').type("47d0e002-9ff5-42d9-b337-73a164a0fe37");
+
+    cy.contains('Submit').click();
   })
 
   beforeEach(() => {
-    cy.viewport(1920, 1080)
     cy.restoreLocalStorageCache()
   })
 
@@ -34,15 +50,18 @@ describe('Timeline', () => {
 
   it('Check the content profile card', () => {
     cy.get('.mcs-profileCard > div:nth-child(2) > div > div:nth-child(2) > div > div.sub-title > div').should('have.text', 'user_id_0')
-    cy.get('.mcs-profileCard > div:nth-child(2) > div > div:nth-child(2) > div > div.custom-object-renderer > span > span > span > div:nth-child(1) > span > span > span').should('have.text', 'name 31')
-    cy.get('.mcs-profileCard > div:nth-child(2) > div > div:nth-child(2) > div > div.custom-object-renderer > span > span > span > div:nth-child(2) > span > span > span').should('have.text', '75008')
-    cy.get('.mcs-profileCard > div:nth-child(2) > div > div:nth-child(2) > div > div.custom-object-renderer > span > span > span > div:nth-child(3) > span > span > span').should('have.text', '75 rue d\'amsterdam')
-    cy.get('.mcs-profileCard > div:nth-child(2) > div > div:nth-child(2) > div > div.custom-object-renderer > span > span > span > div:nth-child(4) > span > span > span').should('have.text', 'France')
-    cy.get('.mcs-profileCard > div:nth-child(2) > div > div:nth-child(2) > div > div.custom-object-renderer > span > span > span > div:nth-child(5)').contains('2020-05-27, 01:10:24')
-    cy.get('.mcs-profileCard > div:nth-child(2) > div > div:nth-child(2) > div > div.custom-object-renderer > span > span > span > button > span').click()
-    cy.get('.mcs-profileCard > div:nth-child(2) > div > div:nth-child(2) > div > div.custom-object-renderer > span > span > span > div:nth-child(6) > span > span > span').should('have.text', '1592')
-    cy.get('.mcs-profileCard > div:nth-child(2) > div > div:nth-child(2) > div > div.custom-object-renderer > span > span > span > div:nth-child(7) > span > span > span').should('have.text', 'last name 31')
-    cy.get('.mcs-profileCard > div:nth-child(2) > div > div:nth-child(2) > div > div.custom-object-renderer > span > span > span > div:nth-child(8)').contains('2020-05-27, 01:10:24')
+
+    cy.get('.mcs-profileCard > div:nth-child(2) > div > div:nth-child(2) > div > div.custom-object-renderer > span > span > span').within(() => {
+      cy.get('div:nth-child(1) > span > span > span').should('have.text', 'name 31')
+      cy.get('div:nth-child(2) > span > span > span').should('have.text', '75008')
+      cy.get('div:nth-child(3) > span > span > span').should('have.text', '75 rue d\'amsterdam')
+      cy.get('div:nth-child(4) > span > span > span').should('have.text', 'France')
+      cy.get('div:nth-child(5)').contains('2020-05-27, 01:10:24')
+      cy.get('button > span').click()
+      cy.get('div:nth-child(6) > span > span > span').should('have.text', '1592')
+      cy.get('div:nth-child(7) > span > span > span').should('have.text', 'last name 31')
+      cy.get('div:nth-child(8)').contains('2020-05-27, 01:10:24')
+    })
   })
 
   it('Check the content of segment card', () => {
@@ -57,6 +76,7 @@ describe('Timeline', () => {
     cy.get('#mcs-main-layout > div.ant-layout > div.ant-layout > div.ant-row-flex.ant-row-flex-space-between.ant-row-flex-middle.mcs-actionbar > div.left-part-margin > button').click()
     cy.get('.ant-select-selection').click()
     cy.get('.ant-select-dropdown-menu > .ant-select-dropdown-menu-item').contains('User Account Id').click()
+    cy.get('.ant-input').clear()
     cy.get('.ant-input').type('user_id_2')
     cy.get('[style="width: 20%;"] > .ant-select-selection').click()
     cy.get('.ant-select-dropdown-menu-item').contains('1592').click()
