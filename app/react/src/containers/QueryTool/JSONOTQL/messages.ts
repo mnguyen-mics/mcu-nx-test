7635a42ed75223735c37e4267ea4faa84e430437
Maxime Leuger <mleuger@mediarithmics.com>
2020-07-30 18:07:08 +0200

    MICS-5849 Warning msg if query_text has been modified
    Warning msg if query_text has been modified

--- app/react/src/containers/QueryTool/JSONOTQL/messages.ts
+++ app/react/src/containers/QueryTool/JSONOTQL/messages.ts
@@ -2,14 +2,22 @@ import { defineMessages, FormattedMessage } from 'react-intl';
 import { FrequencyMode } from './Edit/domain';
 
 export const frequencyModeMessageMap: {
-  [key in FrequencyMode]: FormattedMessage.MessageDescriptor
+  [key in FrequencyMode]: FormattedMessage.MessageDescriptor;
 } = defineMessages({
   AT_LEAST: {
     id: 'objectNode.frequency.mode.at-least',
-    defaultMessage: 'At least'
+    defaultMessage: 'At least',
   },
   AT_MOST: {
     id: 'objectNode.frequency.mode.at-most',
-    defaultMessage: 'At most'
+    defaultMessage: 'At most',
+  },
+});
+
+export const messages = defineMessages({
+  queryHasChanged: {
+    id: 'jsonOtqlFormSection.queryHasChangedWarningMessage',
+    defaultMessage:
+      'Warning: you have edited your query but it is not saved yet. Please click on the Save button to save your modifications.',
   },
 });
