2873869b6131036d73122769a1fed145d7b50edc
Oussama Assassi <oassassi@mediarithmics.com>
2020-08-11 19:30:23 +0200

    MICS-5147 make funnel work for X steps
    number of steps: sky is the limit :)

--- app/react/src/components/Funnel/Funnel.tsx
+++ app/react/src/components/Funnel/Funnel.tsx
@@ -1,16 +1,14 @@
 import * as React from 'react';
-import { Card, Row, Col } from 'antd';
+import { Card } from 'antd';
 import moment from 'moment';
 import { IUserActivitiesFunnelService } from '../../services/UserActivitiesFunnelService';
 import { TYPES } from '../../constants/types';
 import { lazyInject } from '../../config/inversify.config';
-import { FunnelFilter, FunnelTimeRange } from '../../models/datamart/UserActivitiesFunnel';
-
-interface Steps {
-  name: string;
-  count: number;
-  interactionDuration: number;
-}
+import { FunnelFilter, FunnelTimeRange, FunnelResponse } from '../../models/datamart/UserActivitiesFunnel';
+import injectNotifications, {
+  InjectedNotificationProps,
+} from '../../containers/Notifications/injectNotifications';
+import { compose } from 'recompose';
 
 interface StepDelta {
   step: number
@@ -18,22 +16,20 @@ interface StepDelta {
   percentageOfSucceeded?: number;
 }
 
-interface FunnelData {
-  total: number;
-  steps: Steps[];
-}
-
 interface State {
   isLoading: boolean;
-  funnelData: FunnelData;
+  funnelData: FunnelResponse;
   stepDelta: StepDelta[];
   timeRange: FunnelTimeRange;
 }
 
-type Props = {
+type FunnelProps = {
   datamartId: string;
   filter: FunnelFilter;
 }
+
+type Props = FunnelProps &
+  InjectedNotificationProps;
 
 const getPercentage = (nbr: number, total: number): number => {
   return (nbr * 100) / total;
@@ -81,32 +77,39 @@ class Funnel extends React.Component<Props, State> {
       isLoading: true
     });
     return this._userActivitiesFunnelService
-    .getUserActivitiesFunnel(datamartId, filter, timeRange).then((response) => {
-      this.setState({
-        isLoading: false,
-        funnelData: response
-      }, ()=> {
-        setTimeout(() => {
-          this.drawSteps();
+      .getUserActivitiesFunnel(datamartId, filter, timeRange).then((response) => {
+        this.setState({
+          isLoading: false,
+          funnelData: response
+        }, () => {
+          setTimeout(() => {
+            this.drawSteps();
+          });
         });
-      });
-    });
+      })
+      .catch(e => {
+        this.props.notifyError(e);
+        this.setState({
+          isLoading: false,
+        });
+      });;
   }
-  
+
   drawSteps = () => {
     const { funnelData } = this.state;
 
     funnelData.steps.map((step, index) => {
       const start = index === 0 ? funnelData.total : funnelData.steps[index - 1].count;
-      this.drawCanvas((funnelData.total - start), (funnelData.total - step.count), index + 1);
+      this.drawCanvas((funnelData.total - start), (funnelData.total - step.count), index + 1, funnelData.steps.length);
     });
   }
 
-  drawCanvas = (startCount: number, endCount: number, stepNumber: number) => {
+  drawCanvas = (startCount: number, endCount: number, stepNumber: number, totalSteps: number) => {
     const { funnelData } = this.state;
     const container = document.getElementById("container");
     const canvas: any = document.getElementById(`canvas_${stepNumber}`);
-    const drawWidth = container && container.offsetWidth / 3;
+
+    const drawWidth = container && (container.offsetWidth / totalSteps);
     const drawerAreaHeight = 370;
     canvas.width = drawWidth || 0;
 
@@ -137,7 +140,7 @@ class Funnel extends React.Component<Props, State> {
   }
 
   setStepDelta = (stepNumber: number, percentageEnd: number, percentageStart: number) => {
-    this.setState(state => { 
+    this.setState(state => {
       state.stepDelta.push({
         step: stepNumber,
         diff: (stepNumber - 1) !== 2 ? Math.round(percentageEnd) : Math.round(100 - percentageEnd),
@@ -150,64 +153,60 @@ class Funnel extends React.Component<Props, State> {
     });
   }
 
+  isLastStep = (stepNumber: number) => {
+    const { funnelData } = this.state;
+    return funnelData.steps.length === stepNumber;
+  }
+
   render() {
     const { funnelData, stepDelta } = this.state;
+
     return (
       <Card>
-        <div className="mcs-funnel">
-          <Row type="flex"  >
-            <Col span={24} >
-              <div className="mcs-funnel_header">
-                <h1 className="mcs-funnel_header_title">My Funnel name</h1>
-                <button className="mcs-funnel_header_datePicker">
-                  over the last 14 days
+        <div className="mcs-funnel" id="container" >
+          <div className="mcs-funnel_header">
+            <h1 className="mcs-funnel_header_title">My Funnel name</h1>
+            <button className="mcs-funnel_header_datePicker">
+              over the last 14 days
                 </button>
-              </div>
-            </Col>
-          </Row>
-          <Row>
+          </div>
+          <div className="mcs-funnel_steps" >
+
             {funnelData.steps.map((step, index) => {
-              return (<Col span={8} key={index.toString()}>
-                <div className={index === 1 ? "mcs-funnel_stepName mcs-funnel_stepName--middle" : "mcs-funnel_stepName"}>
+              return (<div key={index.toString()} style={{ flex: 1 }} >
+                <div className={"mcs-funnel_stepName"}>
                   <h3 className="mcs-funnel_stepName_title">Step {index + 1}</h3>
                   <p className="mcs-funnel_stepName_desc">{step.name}</p>
                 </div>
-              </Col>)
-            })}
-          </Row>
-          <Row>
-            {funnelData.steps.map((step, index) => {
-              return (<Col span={8} key={index.toString()}>
-                <div className={index === 1 ? "mcs-funnel_userPoints mcs-funnel_userPoints--middle" : "mcs-funnel_userPoints"}>
+                <div className={"mcs-funnel_userPoints"}>
                   <div className="mcs-funnel_userPoints_title">UserPoints</div>
                   <div className="mcs-funnel_userPoints_nbr">{index === 0 ? funnelData.total : step.count}</div>
                 </div>
-              </Col>)
-            })}
-          </Row>
-          <Row type="flex" id="container">
-            {funnelData.steps.map((step, index) => {
-              return (<Col span={8} key={index.toString()}>
-                <div className={index === 1 ? "mcs-funnel_chart mcs-funnel_chart--middle" : "mcs-funnel_chart"}>
-                  {<canvas id={`canvas_${index + 1}`} className={"mcs-funnel_canvas"} height="370" />}
-                  <div className="mcs-funnel_conversionInfo">
-                    <div className={index === 2 ? "mcs-funnel_arrow mcs-funnel_arrow--success" : "mcs-funnel_arrow  mcs-funnel_arrow--failed"} />
-                    <p className="mcs-funnel_stepInfo">
-                      <b>{stepDelta[index] && `${stepDelta[index].diff}%`}</b><br />
-                      <p>{index === 2 ? "Conversions" : "Dropoffs"}</p>
-                    </p>
-                  </div>
+                <div className={"mcs-funnel_chart"}>
                   {stepDelta[index] && stepDelta[index].percentageOfSucceeded && <div className="mcs-funnel_percentageOfSucceeded">
                     <div className="mcs-funnel_arrow mcs_funnel_arrowStep" />
                     <p className="mcs-funnel_stepInfo"><b>{`${stepDelta[index].percentageOfSucceeded}%`}</b> have succeeded in <b>{moment.duration(funnelData.steps[index - 1].interactionDuration, "second").format("d [days] h [hour]")}</b></p>
                   </div>}
+                  {<canvas id={`canvas_${index + 1}`} className={"mcs-funnel_canvas"} height="370" />}
+                  <div className="mcs-funnel_conversionInfo">
+                    <div className={this.isLastStep(index + 1) ? "mcs-funnel_arrow mcs-funnel_arrow--success" : "mcs-funnel_arrow  mcs-funnel_arrow--failed"} />
+                    <p className="mcs-funnel_stepInfo">
+                      <b>{stepDelta[index] && `${stepDelta[index].diff}%`}</b><br />
+                      <p>{this.isLastStep(index + 1) ? "Conversions" : "Dropoffs"}</p>
+                    </p>
+                  </div>
+
                 </div>
-              </Col>)
+              </div>
+              )
             })}
-          </Row>
+
+          </div>
         </div>
-      </Card>)
+      </Card >)
   }
 }
 
-export default Funnel;
+export default compose<FunnelProps, FunnelProps>(
+  injectNotifications,
+)(Funnel);
