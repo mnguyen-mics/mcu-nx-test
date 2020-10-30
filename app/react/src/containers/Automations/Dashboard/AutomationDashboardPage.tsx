0136f9a78a6cdd4331eb4d0adbc2fe64159c8ddc
Gauthier Fontaine <gfontaine@mediarithmics.com>
2020-10-30 09:31:45 +0100

    MICS-6785 Test scenarios
    This commit adds a test button in the action bar of a scenario
    view page, when this scenario is in ACTIVE or PAUSED status.
    
    This button can only be seen with the feature
    automations-test-scenario.
    
    This test button triggers a drawer, which allows to test the user
    point of the user through the scenario. The behavior to find the
    user point of the user is similar to the one used in the timeline
    page. A link to the user point timeline is given in the drawer.
    
    If the user point cannot be found, a default message is shown.
    
    For the moment, the Launch test button (in the drawer) does
    nothing.

--- app/react/src/containers/Automations/Dashboard/AutomationDashboardPage.tsx
+++ app/react/src/containers/Automations/Dashboard/AutomationDashboardPage.tsx
@@ -15,6 +15,14 @@ import { IScenarioService } from '../../../services/ScenarioService';
 import { AutomationStatus } from '../../../models/automations/automations';
 import { Actionbar } from '@mediarithmics-private/mcs-components-library';
 import { Path } from '@mediarithmics-private/mcs-components-library/lib/components/action-bar/Actionbar';
+import { InjectedIntlProps, injectIntl, defineMessages } from 'react-intl';
+import injectDrawer, {
+  InjectedDrawerProps,
+} from '../../../components/Drawer/injectDrawer';
+import AutomationScenarioTest, {
+  AutomationScenarioTestProps,
+} from './Test/AutomationScenarioTest';
+import { InjectedFeaturesProps, injectFeatures } from '../../Features';
 
 export interface AutomationDashboardrams {
   organisationId: string;
@@ -27,9 +35,18 @@ interface State {
   automationFormData: Partial<AutomationFormData>;
 }
 
-type Props = RouteComponentProps<AutomationDashboardrams> &
-  InjectedNotificationProps;
+type Props = InjectedNotificationProps &
+  InjectedDrawerProps &
+  InjectedIntlProps &
+  InjectedFeaturesProps &
+  RouteComponentProps<AutomationDashboardrams>;
 
+const messages = defineMessages({
+  testTitle: {
+    id: 'automationDashboardPage.actionBar.test',
+    defaultMessage: 'Test',
+  },
+});
 
 class AutomationDashboardPage extends React.Component<Props, State> {
   @lazyInject(TYPES.IAutomationFormService)
@@ -101,82 +118,141 @@ class AutomationDashboardPage extends React.Component<Props, State> {
   renderStatus = (status: AutomationStatus) => {
     switch (status) {
       case 'ACTIVE':
-        return <span><McsIcon type="pause" /> Pause</span>
+        return (
+          <span>
+            <McsIcon type="pause" /> Pause
+          </span>
+        );
       case 'NEW':
       case 'PAUSED':
-        return <span><McsIcon type="play" /> Activate</span>
+        return (
+          <span>
+            <McsIcon type="play" /> Activate
+          </span>
+        );
     }
-  }
+  };
 
   onStatusClick = (automationId: string, status: AutomationStatus) => () => {
-    const newStatus: AutomationStatus = status === 'PAUSED' || status === 'NEW' ? 'ACTIVE' : 'PAUSED';
+    const newStatus: AutomationStatus =
+      status === 'PAUSED' || status === 'NEW' ? 'ACTIVE' : 'PAUSED';
     const payload = {
       status: newStatus,
-      id: automationId
-    }
+      id: automationId,
+    };
 
-    this.setState({ updating: true })
-    return this._scenarioService.updateScenario(automationId, payload)
-      .then(r => this.setState({
-        automationFormData: { 
-          ...this.state.automationFormData, 
-          automation: r.data 
-        }, 
-        updating: false  
-      }));
-
-  }
+    this.setState({ updating: true });
+    return this._scenarioService.updateScenario(automationId, payload).then(r =>
+      this.setState({
+        automationFormData: {
+          ...this.state.automationFormData,
+          automation: r.data,
+        },
+        updating: false,
+      }),
+    );
+  };
 
   onEditClick = () => {
     const {
       history,
       match: {
-        params: {
-          organisationId,
-          automationId
-        }
-      }
+        params: { organisationId, automationId },
+      },
     } = this.props;
     history.push(`/v2/o/${organisationId}/automations/${automationId}/edit`);
-  }
+  };
+
+  onTestClick = () => {
+    const { openNextDrawer, closeNextDrawer } = this.props;
+    const size: 'small' | 'large' = 'small';
+
+    const props = {
+      close: closeNextDrawer,
+    };
+
+    openNextDrawer<AutomationScenarioTestProps>(AutomationScenarioTest, {
+      additionalProps: props,
+      size: size,
+    });
+  };
 
   render() {
-    const { match: { params: { organisationId } } } = this.props;
-    const { automationFormData, isLoading, updating, } = this.state;
+    const {
+      match: {
+        params: { organisationId },
+      },
+      intl: { formatMessage },
+      hasFeature,
+    } = this.props;
+    const { automationFormData, isLoading, updating } = this.state;
 
     if (isLoading) {
       return <Loading className="loading-full-screen" />;
     }
 
-    if (!automationFormData.automation || !automationFormData.automationTreeData) {
-      return ('this automation does not seem to exist!')
+    if (
+      !automationFormData.automation ||
+      !automationFormData.automationTreeData
+    ) {
+      return 'this automation does not seem to exist!';
     }
 
     const breadCrumbPaths: Path[] = [
       {
         name: 'Automations',
-        path: `/v2/o/${organisationId}/automations`
+        path: `/v2/o/${organisationId}/automations`,
       },
       {
-        name: automationFormData.automation.name ? automationFormData.automation.name : ''
-      }
+        name: automationFormData.automation.name
+          ? automationFormData.automation.name
+          : '',
+      },
     ];
 
+    const testButton =
+      automationFormData.automation &&
+      automationFormData.automation.status &&
+      (automationFormData.automation.status === 'ACTIVE' ||
+        automationFormData.automation.status === 'PAUSED') &&
+      hasFeature('automations-test-scenario') ? (
+        <Button onClick={this.onTestClick}>
+          <McsIcon type={'gears'} />
+          {formatMessage(messages.testTitle)}
+        </Button>
+      ) : null;
 
     return (
       <div style={{ height: '100%', display: 'flex' }}>
         <Layout>
-          <Actionbar
-            paths={breadCrumbPaths}
-          >
-            {automationFormData.automation && automationFormData.automation.status && automationFormData.automation.id ? <Button onClick={this.onStatusClick(automationFormData.automation.id, automationFormData.automation.status)} className={"mcs-primary"} type="primary">
-              {updating ? <i className="mcs-table-cell-loading" style={{ minWidth: 50}} /> : this.renderStatus(automationFormData.automation.status)}
-            </Button> : null}
+          <Actionbar paths={breadCrumbPaths}>
+            {automationFormData.automation &&
+            automationFormData.automation.status &&
+            automationFormData.automation.id ? (
+              <Button
+                onClick={this.onStatusClick(
+                  automationFormData.automation.id,
+                  automationFormData.automation.status,
+                )}
+                className={'mcs-primary'}
+                type="primary"
+              >
+                {updating ? (
+                  <i
+                    className="mcs-table-cell-loading"
+                    style={{ minWidth: 50 }}
+                  />
+                ) : (
+                  this.renderStatus(automationFormData.automation.status)
+                )}
+              </Button>
+            ) : null}
             <Button onClick={this.onEditClick}>
-              <McsIcon type={"pen"} /> Edit
+              <McsIcon type={'pen'} /> Edit
             </Button>
+            {testButton}
           </Actionbar>
-          
+
           <Layout.Content
             className={`mcs-content-container`}
             style={{ padding: 0, overflow: 'hidden' }}
@@ -187,7 +263,13 @@ class AutomationDashboardPage extends React.Component<Props, State> {
               exitCondition={automationFormData.exitCondition}
               scenarioId={automationFormData.automation.id!}
               viewer={true}
-              creation_mode={automationFormData.automationTreeData && automationFormData.automationTreeData.node.type === 'QUERY_INPUT' ? automationFormData.automationTreeData.node.ui_creation_mode : 'QUERY'}
+              creation_mode={
+                automationFormData.automationTreeData &&
+                automationFormData.automationTreeData.node.type ===
+                  'QUERY_INPUT'
+                  ? automationFormData.automationTreeData.node.ui_creation_mode
+                  : 'QUERY'
+              }
             />
           </Layout.Content>
         </Layout>
@@ -197,6 +279,9 @@ class AutomationDashboardPage extends React.Component<Props, State> {
 }
 
 export default compose(
-	withRouter,
+  withRouter,
+  injectIntl,
+  injectDrawer,
   injectNotifications,
+  injectFeatures,
 )(AutomationDashboardPage);
---
+++ app/react/src/containers/Automations/Dashboard/Test/AutomationScenarioTest.tsx
@@ -0,0 +1,205 @@
+import { Button, Layout, Spin } from 'antd';
+import * as React from 'react';
+import { FormattedMessage, InjectedIntlProps, injectIntl } from 'react-intl';
+import { connect } from 'react-redux';
+import { Link, RouteComponentProps, withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
+import { FormLayoutActionbar } from '../../../../components/Layout';
+import { lazyInject } from '../../../../config/inversify.config';
+import { TYPES } from '../../../../constants/types';
+import { UserWorkspaceResource } from '../../../../models/directory/UserProfileResource';
+import {
+  Cookies,
+  isUserPointIdentifier,
+} from '../../../../models/timeline/timeline';
+import { getWorkspace } from '../../../../redux/Session/selectors';
+import { IScenarioService } from '../../../../services/ScenarioService';
+import { IUserDataService } from '../../../../services/UserDataService';
+import { MicsReduxState } from '../../../../utils/ReduxHelper';
+import injectNotifications, {
+  InjectedNotificationProps,
+} from '../../../Notifications/injectNotifications';
+import messages from './messages';
+
+const { Content } = Layout;
+
+export interface AutomationScenarioTestParams {
+  organisationId: string;
+  automationId: string;
+}
+
+export interface AutomationScenarioTestProps {
+  close: () => void;
+}
+
+interface MapStateToProps {
+  cookies: Cookies;
+  workspace: (organisationId: string) => UserWorkspaceResource;
+}
+
+type Props = MapStateToProps &
+  AutomationScenarioTestProps &
+  InjectedIntlProps &
+  InjectedNotificationProps &
+  RouteComponentProps<AutomationScenarioTestParams>;
+
+interface State {
+  isFetchingUserPointId: boolean;
+  userPointId?: string;
+}
+
+class AutomationScenarioTest extends React.Component<Props, State> {
+  @lazyInject(TYPES.IScenarioService)
+  private _scenarioService: IScenarioService;
+
+  @lazyInject(TYPES.IUserDataService)
+  private _userDataService: IUserDataService;
+
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      isFetchingUserPointId: false,
+    };
+  }
+
+  componentDidMount() {
+    const { cookies } = this.props;
+
+    if (cookies.mics_vid) {
+      this.fetchUserPointId(cookies.mics_vid);
+    }
+  }
+
+  fetchUserPointId = (micsVid: string) => {
+    const {
+      match: {
+        params: { organisationId, automationId },
+      },
+      notifyError,
+    } = this.props;
+
+    this.setState({ isFetchingUserPointId: true }, () => {
+      this._scenarioService
+        .getScenario(automationId)
+        .then(resAutomationResource => {
+          const datamartId = resAutomationResource.data.datamart_id;
+
+          return this._userDataService
+            .getIdentifiers(
+              organisationId,
+              datamartId,
+              'user_agent_id',
+              `vec:${micsVid}`,
+            )
+            .then(resUserIdentifierInfo => {
+              this.setState({
+                isFetchingUserPointId: false,
+                userPointId: resUserIdentifierInfo.data.find(
+                  isUserPointIdentifier,
+                )?.user_point_id,
+              });
+            });
+        })
+        .catch(err => {
+          notifyError(err);
+          this.setState({ isFetchingUserPointId: false });
+        });
+    });
+  };
+
+  getContent = () => {
+    const {
+      cookies,
+      match: {
+        params: { organisationId },
+      },
+    } = this.props;
+
+    const { userPointId } = this.state;
+
+    const displayedId = userPointId
+      ? userPointId
+      : cookies.mics_vid
+      ? `vec:${cookies.mics_vid}`
+      : '';
+
+    const subtitle = cookies.mics_vid ? (
+      <div className="subtitle">
+        <FormattedMessage {...messages.contentSubtitleBegin} />
+        <Link
+          to={`/v2/o/${organisationId}/audience/timeline/user_agent_id/vec:${cookies.mics_vid}`}
+          target="_blank"
+        >
+          {displayedId}
+        </Link>
+        <FormattedMessage {...messages.contentSubtitleEnd} />
+        <div className="edit-top">
+          <Button className={'mcs-primary'} type="primary">
+            <FormattedMessage {...messages.buttonTitle} />
+          </Button>
+        </div>
+      </div>
+    ) : (
+      <div className="subtitle">
+        <FormattedMessage {...messages.contentSubtitleNoUserPoint} />
+      </div>
+    );
+
+    return (
+      <div className="title-container">
+        <div className="title">
+          <FormattedMessage {...messages.contentTitle} />
+        </div>
+        {subtitle}
+      </div>
+    );
+  };
+
+  render() {
+    const {
+      intl: { formatMessage },
+      close,
+    } = this.props;
+
+    const { isFetchingUserPointId } = this.state;
+
+    const breadcrumbPaths = [
+      {
+        name: formatMessage(messages.testActionbarTitle),
+      },
+    ];
+
+    const actionBarProps = {
+      paths: breadcrumbPaths,
+      onClose: close,
+    };
+
+    const content = isFetchingUserPointId ? (
+      <Spin size="small" className="text-center" />
+    ) : (
+      this.getContent()
+    );
+
+    return (
+      <Layout className="ant-layout">
+        <FormLayoutActionbar {...actionBarProps} />
+        <Content className="mcs-content-container mcs-form-container">
+          {content}
+        </Content>
+      </Layout>
+    );
+  }
+}
+
+const mapStateToProps = (state: MicsReduxState) => ({
+  cookies: state.session.cookies,
+  workspace: getWorkspace(state),
+});
+
+export default compose<Props, AutomationScenarioTestProps>(
+  injectIntl,
+  withRouter,
+  injectNotifications,
+  connect(mapStateToProps, undefined),
+)(AutomationScenarioTest);
