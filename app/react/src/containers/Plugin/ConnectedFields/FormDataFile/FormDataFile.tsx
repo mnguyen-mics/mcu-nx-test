3be48aaed8c9c6ad5ac751574061ebd77f448c01
Oussama Assassi <oassassi@mediarithmics.com>
2021-01-12 17:20:40 +0100

    MICS-7676 sanitize html content return by api
    sanitize html content return by api to avoid XSS attack

--- app/react/src/containers/Plugin/ConnectedFields/FormDataFile/FormDataFile.tsx
+++ app/react/src/containers/Plugin/ConnectedFields/FormDataFile/FormDataFile.tsx
@@ -18,7 +18,7 @@ import { InjectedDrawerProps } from '../../../../components/Drawer/injectDrawer'
 import { lazyInject } from '../../../../config/inversify.config';
 import { IDataFileService } from '../../../../services/DataFileService';
 import { TYPES } from '../../../../constants/types';
-
+import DOMPurify from 'dompurify';
 
 export type AcceptedFile = 'text/html' | '*';
 
@@ -80,15 +80,22 @@ class FormDataFile extends React.Component<JoinedProps, FormDataFileState> {
       this.onFileUpdate(res).then((fileContent: string) => {
         const fileName = this.parseFileName(uri);
         const basePath = this.parseFileName(uri, true);
+
+        const sanitizedContent =  DOMPurify.sanitize(
+          fileContent, 
+          {
+            SAFE_FOR_TEMPLATES: true, 
+            WHOLE_DOCUMENT: true
+          });
         this.setState({
           canEdit: true,
-          fileContent: fileContent,
+          fileContent: sanitizedContent,
           fileName: fileName,
           basePath: basePath,
         });
         input.onChange({
           uri: uri,
-          fileContent: fileContent,
+          fileContent: sanitizedContent,
           fileName: fileName,
         });
       });
