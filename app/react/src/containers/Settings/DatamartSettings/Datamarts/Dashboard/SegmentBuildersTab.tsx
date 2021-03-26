8cdc8701c29a5274fc8f21bbfe29a4efaec3c349
mlivney <mlivney@mediarithmics.com>
2021-03-26 11:20:17 +0100

    MICS-8705 Rename Audience Builder elements in settings
    Rename and separate Audience Builder elements in settings to
    Audience Features and Segment Builders.

---
+++ app/react/src/containers/Settings/DatamartSettings/Datamarts/Dashboard/SegmentBuildersTab.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import AudienceBuilderListPage from '../../AudienceBuilder/List/AudienceBuilderListPage';
+import { injectFeatures, InjectedFeaturesProps } from '../../../../Features';
+import { compose } from 'recompose';
+
+class SegmentBuildersTab extends React.Component<InjectedFeaturesProps> {
+  public render() {
+    return (
+      <div>
+        <AudienceBuilderListPage />
+      </div>
+    );
+  }
+}
+
+export default compose<InjectedFeaturesProps, {}>(injectFeatures)(
+  SegmentBuildersTab,
+);
