9fa877fe8d4a7b5158f6860285729b276ddb071a
mckongkong <mckongkong@mediarithmics.com>
2022-02-08 14:28:00 +0100

    MICS-10596 : Test reset password
    - Check reset password expiration form
    - Update `Keycloak_errors.spec.ts`

--- Cypress/cypress/integration/Keycloak/Keycloak_errors.spec.ts
+++ Cypress/cypress/integration/Keycloak/Keycloak_errors.spec.ts
@@ -9,20 +9,25 @@ describe('Should not access the platform when using bad credentials', () => {
   });
 
   it('Login with bad credentials', () => {
+    cy.visit('/');
     // Login with bad email
-    cy.kcLogin('badEmail');
-    cy.get('.input-error').should('be.visible').and('contain', 'Invalid username or password');
+    cy.get('#username').type('badEmail');
+    cy.get('#kc-login').click();
+    cy.get('.input-error').should('be.visible').and('contain', 'Invalid email');
 
     // Login with bad password
     cy.get('#username').type('{selectall}{backspace}' + `${Cypress.env('devMail')}`);
+    cy.get('#kc-login').click();
     cy.get('#password').type('{selectall}{backspace}qsdfjdsqN7@kfeu');
     cy.get('#kc-login').click();
-    cy.get('.input-error').should('be.visible').and('contain', 'Invalid username or password');
+    cy.get('.input-error').should('be.visible').and('contain', 'Invalid password');
 
     // Login with bad email and bad password
+    cy.get('#reset-login').click();
     cy.get('#username').type('{selectall}{backspace}' + 'sdfsdf@mediarithmics.com');
+    cy.get('#kc-login').click();
     cy.get('#password').type('{selectall}{backspace}qsdfjdsqN7@kfeu');
     cy.get('#kc-login').click();
-    cy.get('.input-error').should('be.visible').and('contain', 'Invalid username or password');
+    cy.get('.input-error').should('be.visible').and('contain', 'Invalid password');
   });
 });
