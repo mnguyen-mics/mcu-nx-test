622ede11582455c14d1179a313df569d5a84bec2
hkounka <hkounka@mediarithmics.com>
2022-02-11 17:11:54 +0100

    MICS-12228 : add e2e on the ability in advanced segment builder
    to open the query of an existing segment
    This cypress tests the option to open an existing
    segment created with advanced segment builder

--- Cypress/cypress/integration/SegmentBuilder/AdvancedSegmentBuilder.spec.ts
+++ Cypress/cypress/integration/SegmentBuilder/AdvancedSegmentBuilder.spec.ts
@@ -61,4 +61,42 @@ describe('The purpose of this test is to check the creation of a segment with th
         .and('contain.text', '@ScoreSum(min: ' + frequency_value);
     });
   });
+
+  it('Should test the open from existing segment on advanced segment builder', () => {
+    cy.get('.mcs-sideBar-subMenu_menu\\.audience\\.title').click();
+    cy.get('.mcs-sideBar-subMenuItem_menu\\.audience\\.builder').click();
+    cy.get('.mcs-advancedSegmentBuilder').click();
+    cy.get('.mcs-plusNodeWidget_button').click();
+    cy.get('.mcs-plusNodeWidget_menu_object').click();
+    cy.get('.mcs-objectNodeSection_select').click();
+    cy.get('.mcs-select_itemOption--accounts').click();
+    cy.get('.mcs-objectNodeSection_checkbox').click();
+    const frequencyValue = faker.random.number().toString();
+    cy.get('.mcs-objectNodeSection_frequency_value').click().type(frequencyValue);
+    cy.get('.mcs-form_saveButton_queryDocumentForm').click();
+    cy.get('.mcs-saveQueryAsActionBar_button').click();
+    cy.get('.mcs-saveQueryAsActionBar_menu_userQuery').click();
+    const segmentName = faker.random.number().toString();
+    cy.get('.mcs-newUserQuerySegmentSimpleForm_name_input').type(segmentName);
+    cy.get('.mcs-saveAsUserQuerySegmentModal_ok_button').click();
+    cy.url().should('match', /.*segments\/[0-9]+/);
+    cy.url().then(url => {
+      const segmentId: number = parseInt(
+        url.substring(url.indexOf('segments') + 9, url.length),
+        10,
+      );
+      cy.get('.mcs-sideBar-subMenuItem_menu\\.audience\\.builder').click();
+      cy.get('.mcs-advancedSegmentBuilder').click();
+      cy.get('.mcs-segmentSelector_button').click();
+      cy.get('.mcs-chartMetaDataInfo_container').should('contain', segmentName);
+      cy.get('.mcs-segmentSelector_dropdown').find('input').clear().type(segmentName);
+      cy.wait(5000);
+      cy.get('.mcs-chartMetaDataInfo_container').should('contain', segmentName);
+      cy.get('.mcs-segmentSelector_dropdown').find('input').clear().type(`${segmentId}`);
+      cy.wait(5000);
+      cy.get('.mcs-chartMetaDataInfo_container').should('contain', segmentName);
+      cy.contains(segmentName).click();
+      cy.get('.query-builder').should('contain', frequencyValue);
+    });
+  });
 });
