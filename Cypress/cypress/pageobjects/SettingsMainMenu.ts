32aa9a993b39862702a100e140af0a63c965c971
haroubate <haroubate@mediarithmics.com>
2022-06-01 17:31:47 +0200

    MICS-13449 Create POM for SettingsMainMenu & update actionbar.ts
    create SettignsMainMenu.ts and add possibles actions
    
    update actionbar.ts (change name to header.ts) and Logout.spec.ts

---
+++ Cypress/cypress/pageobjects/SettingsMainMenu.ts
@@ -0,0 +1,89 @@
+import Page from './Page';
+
+class SettingsMainMenu extends Page {
+  get settingsMainMenuContainer() {
+    return cy.get('.mcs-settingsMainMenu_container');
+  }
+
+  get myAccount() {
+    return cy.get('.mcs-settingsMainMenu_menu\\.account\\.title');
+  }
+
+  get organisation() {
+    return cy.get('.mcs-settingsMainMenu_menu\\.organisation\\.title');
+  }
+
+  get datamart() {
+    return cy.get('.mcs-settingsMainMenu_menu\\.datamart\\.title');
+  }
+
+  get campaigns() {
+    return cy.get('.mcs-settingsMainMenu_menu\\.campaigns\\.title');
+  }
+
+  get services() {
+    return cy.get('.mcs-settingsMainMenu_menu\\.service\\.offers\\.title');
+  }
+
+  get arrowMenu() {
+    return cy.get('.mcs-settingsMainMenu_container_arrowMenu');
+  }
+
+  get settingsMainMenu() {
+    return cy.get('.mcs-settings-main-menu');
+  }
+
+  clickMyAccount() {
+    this.myAccount.click();
+  }
+
+  clickOrganisation() {
+    this.organisation.click();
+  }
+
+  clickDatamart() {
+    this.datamart.click();
+  }
+
+  clickCampaigns() {
+    this.campaigns.click();
+  }
+
+  clickServices() {
+    this.services.click();
+  }
+
+  clickArrowMenu() {
+    this.arrowMenu.click();
+  }
+
+  clickHomeMenu() {
+    cy.get('.mcs-sideBar-menuItem_simple').click();
+  }
+
+  clickAudienceMenu() {
+    cy.get('.mcs-sideBar-subMenu_menu\\.audience\\.title').click();
+  }
+
+  clickCampaignsMenu() {
+    cy.get('.mcs-sideBar-subMenu_menu\\.campaign\\.title').click();
+  }
+
+  clickAutomationsMenu() {
+    cy.get('.mcs-sideBar-subMenu_menu\\.automation\\.title').click();
+  }
+
+  clickCreativesMenu() {
+    cy.get('.mcs-sideBar-subMenu_menu\\.library\\.title').click();
+  }
+
+  clickLibraryMenu() {
+    cy.get('.mcs-sideBar-subMenu_menu\\.library\\.title').click();
+  }
+
+  clickDataStudioMenu() {
+    cy.get('.mcs-sideBar-subMenu_menu\\.dataStudio\\.title').click();
+  }
+}
+
+export default new SettingsMainMenu();
