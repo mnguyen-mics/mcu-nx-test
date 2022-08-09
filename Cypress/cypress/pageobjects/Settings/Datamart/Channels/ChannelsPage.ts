aa7d6810fcf46900164fa2ebae7de6b8d04ae226
haroubate <haroubate@mediarithmics.com>
2022-08-09 11:37:53 +0200

    MICS-14035 Fix Cypress Test Datamart
    Update POM channelsPage.ts

--- Cypress/cypress/pageobjects/Settings/Datamart/Channels/ChannelsPage.ts
+++ Cypress/cypress/pageobjects/Settings/Datamart/Channels/ChannelsPage.ts
@@ -24,6 +24,7 @@ class ChannelsPage extends Page {
 
   @logFunction()
   goToPage() {
+    HeaderMenu.clickSettingsIcon();
     SettingsMenu.clickDatamart();
     DatamartMenu.clickChannels();
   }
