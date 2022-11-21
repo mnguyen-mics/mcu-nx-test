99c92ac2ec4c00005803bbd53808b1d473669a09
Maxime Nguyen <mnguyen.ext@mediarithmics.com>
2022-11-21 14:47:44 +0100

    MICS-14836 Add button to clear tabs in query tool
    Add button to clear tabs in query tool

--- app/react/src/containers/QueryTool/OTQL/QueryToolTabsContainer.tsx
+++ app/react/src/containers/QueryTool/OTQL/QueryToolTabsContainer.tsx
@@ -66,6 +66,10 @@ const messages = defineMessages({
     id: 'queryTool.OtqlConsole.tab.chartShared',
     defaultMessage: 'The chart URL is copied to the clipboard',
   },
+  clearTabs: {
+    id: 'queryTool.OtqlConsole.tab.clearTabs',
+    defaultMessage: 'Clear tabs',
+  },
 });
 
 const { Content } = Layout;
@@ -1040,6 +1044,11 @@ class QueryToolTabsContainer extends React.Component<Props, State> {
     return `${window.location.host}/#${location.pathname}?chartId=${chartId}`;
   };
 
+  clearAllSavedQueries = () => {
+    localStorage.removeItem('savedQueries');
+    this.initTabState();
+  };
+
   render() {
     const {
       match: {
@@ -1076,6 +1085,9 @@ class QueryToolTabsContainer extends React.Component<Props, State> {
 
     const saveExtraOptions = (
       <Row>
+        <Button className='mcs-otqlInputEditor_save_as_button' onClick={this.clearAllSavedQueries}>
+          {intl.formatMessage(messages.clearTabs)}
+        </Button>
         {currentTab?.chartItem && (
           <CopyToClipboard
             onCopy={this.handleShareButtonClick}
